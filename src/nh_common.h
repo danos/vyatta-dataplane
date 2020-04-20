577b28104efc41a4ce687f5f3846e8707c3e073d
Paul Atkins <paul.atkins@intl.att.com>
2020-04-20 13:06:01 +0100

    nh_common: move struct next_hop_u into nh_common.h
    VRVDR-50724

--- src/nh_common.h
+++ src/nh_common.h
@@ -9,6 +9,10 @@
 #include <netinet/in.h>
 #include <netinet/in.h>
 
+#include "fal_plugin.h"
+#include "pd_show.h"
+#include "urcu.h"
+
 struct ifnet;
 struct llentry;
 
@@ -25,5 +29,24 @@ struct next_hop {
 		struct in6_addr gateway6; /* nexthop IPv6 address */
 	};
 };
+
+/*
+ * This is the nexthop information result of route lookup - allows for
+ * multiple nexthops in the case of ECMP
+ */
+struct next_hop_u {
+	struct next_hop      *siblings;	/* array of next_hop */
+	uint8_t              nsiblings;	/* # of next_hops */
+	uint8_t              proto;	/* routing protocol */
+	uint16_t             padding;
+	uint32_t             index;
+	struct next_hop      hop0;      /* optimization for non-ECMP */
+	uint32_t             refcount;	/* # of LPM's referring */
+	enum pd_obj_state    pd_state;
+	struct cds_lfht_node nh_node;
+	fal_object_t         nhg_fal_obj;   /* FAL handle for next_hop_group */
+	fal_object_t         *nh_fal_obj; /* Per-nh FAL handles */
+	struct rcu_head      rcu;
+} __rte_cache_aligned;
 
 #endif /* NH_COMMON_H */
