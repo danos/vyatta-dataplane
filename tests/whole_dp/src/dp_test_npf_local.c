7c9621f63bf723918836a83822df4c84930c80c8
ak487r <alexey.khan@intl.att.com>
2020-05-12 14:32:25 -0400

    tests: add ipv4 icmp packet to big tests for originating firewall
    Test creates ipv4 icmp packet with DF bit set and route it
    to dataplane interface that has mtu less than the packet size.
    Router creates and sends ipv4 icmp packet to big message back.
    Originate firewall is configured in the output interface to verify
    dscp mark function and action drop.
    
    VRVDR-49186

--- tests/whole_dp/src/dp_test_npf_local.c
+++ tests/whole_dp/src/dp_test_npf_local.c
@@ -509,3 +509,191 @@ DP_START_TEST(ipv6_tcp_shadow, drop)
 
 	dp_test_nl_del_ip_addr_and_connected("dp1T0", "2001::1/64");
 } DP_END_TEST;
+
+DP_DECL_TEST_CASE(npf_orig, ipv4_icmp_transit, NULL, NULL);
+
+/*
+ * Match on ICMP type and code
+ * Test generate ICMP message upon packet too big with don't fragment
+ * flag set
+ *
+ *                  2.2.2.2 +-----+ 1.1.1.1
+ *                          |     |
+ * host 2.2.2.1 ------------| uut |---------------host 1.1.1.2
+ *                    dp3T3 |     | dp1T1 (mtu 1400)
+ *                    intf1 +-----+ intf2
+ *
+ *
+ *              --> Forwards (on output)
+ *              Source 2.2.2.1 Destination 1.1.1.2 (length 1472, DSCP 0)
+ *
+ *                <-- Back ICMP
+ *              Source 1.1.1.2 Destination 2.2.2.2
+ */
+static void npf_orig_ipv4_icmp_transit_setup(
+		struct dp_test_expected **exp,
+		struct rte_mbuf **test_pak)
+{
+	struct rte_mbuf *icmp_pak;
+	const char *neigh3_mac_str = "aa:bb:cc:dd:ee:ff";
+	const char *neigh1_mac_str = "bb:aa:cc:ee:dd:ff";
+	struct iphdr *ip_inner;
+	struct icmphdr *icph;
+	struct iphdr *ip;
+	int len = 1472;
+	int icmplen;
+
+	/* Set up the interface addresses */
+	dp_test_nl_add_ip_addr_and_connected("dp1T1", "1.1.1.1/24");
+	dp_test_nl_add_ip_addr_and_connected("dp3T3", "2.2.2.2/24");
+
+	dp_test_netlink_set_interface_mtu("dp1T1", 1400);
+
+	/* Add the nh arp we want the packet to follow */
+	dp_test_netlink_add_neigh("dp3T3", "2.2.2.1", neigh3_mac_str);
+	dp_test_netlink_add_neigh("dp1T1", "1.1.1.2", neigh1_mac_str);
+
+	/* Create pak to match the route added above */
+	*test_pak = dp_test_create_ipv4_pak("2.2.2.1", "1.1.1.2",
+					   1, &len);
+	ip = iphdr(*test_pak);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_DF, 1);
+
+	(void)dp_test_pktmbuf_eth_init(*test_pak,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       neigh3_mac_str, RTE_ETHER_TYPE_IPV4);
+
+	/*
+	 * Expected packet
+	 */
+	/* Create expected icmp packet  */
+	icmplen = sizeof(struct iphdr) + 576;
+	icmp_pak = dp_test_create_icmp_ipv4_pak("2.2.2.2", "2.2.2.1",
+						ICMP_DEST_UNREACH,
+						ICMP_FRAG_NEEDED,
+						DPT_ICMP_FRAG_DATA(1400),
+						1, &icmplen,
+						iphdr(*test_pak),
+						&ip, &icph);
+
+	(void)dp_test_pktmbuf_eth_init(icmp_pak,
+				       neigh3_mac_str,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       RTE_ETHER_TYPE_IPV4);
+
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TOS,
+				 IPTOS_PREC_INTERNETCONTROL);
+
+	ip_inner = (struct iphdr *)(icph + 1);
+	/*
+	 * The TTL allowed to be changed from the original. From RFC
+	 * 1812 s4.3.2.3:
+	 *   The returned IP header (and user data) MUST be identical to
+	 *   that which was received, except that the router is not
+	 *   required to undo any modifications to the IP header that are
+	 *   normally performed in forwarding that were performed before
+	 *   the error was detected (e.g., decrementing the TTL, or
+	 *   updating options)
+	 */
+	dp_test_set_pak_ip_field(ip_inner, DP_TEST_SET_TTL,
+				 DP_TEST_PAK_DEFAULT_TTL - 1);
+
+	ip = iphdr(icmp_pak);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TOS, IPTOS_DSCP_AF12);
+
+	*exp = dp_test_exp_create(icmp_pak);
+	rte_pktmbuf_free(icmp_pak);
+}
+
+DP_START_TEST(ipv4_icmp_transit, packet_to_big_dscp_remark)
+{
+	struct dp_test_expected *exp;
+	struct rte_mbuf *test_pak;
+
+	npf_orig_ipv4_icmp_transit_setup(&exp, &test_pak);
+
+	struct dp_test_npf_rule_t rules[] = {
+		{
+			.rule     = "1",
+			.pass     = PASS,
+			.stateful = STATELESS,
+			.npf      = "proto-final=1 rproc=markdscp(12)"},
+		RULE_DEF_BLOCK,
+		NULL_RULE };
+
+	struct dp_test_npf_ruleset_t fw = {
+		.rstype = "originate",
+		.name   = "FW_ICMPv4_ORIG",
+		.enable = 1,
+		.attach_point   = "dp3T3",
+		.fwd    = FWD,
+		.dir    = "out",
+		.rules  = rules
+	};
+	dp_test_npf_fw_add(&fw, false);
+
+	dp_test_exp_set_oif_name(exp, "dp3T3");
+
+	/* now send test pak and check we get expected back */
+	dp_test_pak_receive(test_pak, "dp3T3", exp);
+
+	/* After test validations */
+	dp_test_npf_verify_rule_pkt_count(NULL, &fw, fw.rules[0].rule, 1);
+
+	/* Clean Up */
+	dp_test_npf_fw_del(&fw, false);
+
+	dp_test_netlink_del_neigh("dp3T3", "2.2.2.1", "aa:bb:cc:dd:ee:ff");
+	dp_test_netlink_del_neigh("dp1T1", "1.1.1.2", "bb:aa:cc:ee:dd:ff");
+	dp_test_nl_del_ip_addr_and_connected("dp1T1", "1.1.1.1/24");
+	dp_test_nl_del_ip_addr_and_connected("dp3T3", "2.2.2.2/24");
+
+	dp_test_netlink_set_interface_mtu("dp1T1", 1500);
+} DP_END_TEST;
+
+DP_START_TEST(ipv4_icmp_transit, drop)
+{
+	struct dp_test_expected *exp;
+	struct rte_mbuf *test_pak;
+
+	npf_orig_ipv4_icmp_transit_setup(&exp, &test_pak);
+
+	struct dp_test_npf_rule_t rules[] = {
+		{
+			.rule     = "1",
+			.pass     = BLOCK,
+			.stateful = STATELESS,
+			.npf      = "proto-final=1 rproc=markdscp(12)"},
+		RULE_DEF_PASS,
+		NULL_RULE };
+
+	struct dp_test_npf_ruleset_t fw = {
+		.rstype = "originate",
+		.name   = "FW_ICMPv4_ORIG",
+		.enable = 1,
+		.attach_point   = "dp3T3",
+		.fwd    = FWD,
+		.dir    = "out",
+		.rules  = rules
+	};
+	dp_test_npf_fw_add(&fw, false);
+
+	dp_test_exp_set_oif_name(exp, "dp3T3");
+	dp_test_exp_set_fwd_status(exp, DP_TEST_FWD_DROPPED);
+
+	/* Run test */
+	dp_test_pak_receive(test_pak, "dp3T3", exp);
+
+	/* After test validations */
+	dp_test_npf_verify_rule_pkt_count(NULL, &fw, fw.rules[0].rule, 1);
+
+	/* Clean Up */
+	dp_test_npf_fw_del(&fw, false);
+
+	dp_test_netlink_del_neigh("dp3T3", "2.2.2.1", "aa:bb:cc:dd:ee:ff");
+	dp_test_netlink_del_neigh("dp1T1", "1.1.1.2", "bb:aa:cc:ee:dd:ff");
+	dp_test_nl_del_ip_addr_and_connected("dp1T1", "1.1.1.1/24");
+	dp_test_nl_del_ip_addr_and_connected("dp3T3", "2.2.2.2/24");
+
+	dp_test_netlink_set_interface_mtu("dp1T1", 1500);
+} DP_END_TEST;
