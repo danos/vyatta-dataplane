669db32c627a3627a9c14b2a45f1db79c026a9af
Paul Atkins <paul.atkins@intl.att.com>
2020-05-16 11:19:50 +0100

    ut: add an ipv6 pic edge test
    As almost all the pic edge code is af independent there is no need
    to replicate all the v4 tests.  However there is value in adding
    a simple test to check the parts of the code that are different
    such as the hashing to find the next hops.

--- tests/whole_dp/src/dp_test_ip_pic_edge.c
+++ tests/whole_dp/src/dp_test_ip_pic_edge.c
@@ -56,8 +56,13 @@ static void _dp_test_verify_nh_map_count(const char *addr,
 {
 	char cmd[100];
 	char expected[DP_TEST_TMP_BUF];
+	struct dp_test_addr addr_ptr;
 
-	snprintf(cmd, sizeof(cmd), "route lookup %s", addr);
+	dp_test_assert_internal(dp_test_addr_str_to_addr(addr,  &addr_ptr));
+	if (addr_ptr.family == AF_INET)
+		snprintf(cmd, sizeof(cmd), "route lookup %s", addr);
+	else
+		snprintf(cmd, sizeof(cmd), "route6 lookup %s", addr);
 	dp_test_map_count_build_expected(expected, sizeof(expected),
 					 addr, count, list);
 
@@ -791,3 +796,61 @@ DP_START_TEST(ip_pic_edge10, ip_pic_edge10)
 	dp_test_clear_path_unusable();
 
 } DP_END_TEST;
+
+
+/*
+ * Create multiple NHs and check that all are marked unusable when
+ * the update signal arrives.
+ *
+ * This is the IPv6 version of ip_pic_edge5. As almost all the code
+ * being tested here is AF independent we only need minimal
+ * tests to check the differences in the hashing.
+ */
+DP_DECL_TEST_CASE(ip_pic_edge_suite, ip_pic_edge11, NULL, NULL);
+DP_START_TEST(ip_pic_edge11, ip_pic_edge11)
+{
+	int map_list1[] = { 0 };
+	int map_list2[] = { 1 };
+
+	dp_test_nl_add_ip_addr_and_connected("dp1T1", "1::1/64");
+	dp_test_nl_add_ip_addr_and_connected("dp2T1", "2::2/64");
+	dp_test_nl_add_ip_addr_and_connected("dp3T1", "3::3/64");
+
+	dp_test_netlink_add_route(
+		"10::/64 "
+		"nh 1::2 int:dp1T1 "
+		"nh 2::1 int:dp2T1 backup");
+
+	dp_test_netlink_add_route(
+		"10:1::/64 "
+		"nh 1::3 int:dp1T1 "
+		"nh 2::3 int:dp2T1 backup");
+
+	dp_test_verify_nh_map_count("10::1", 1, map_list1);
+	dp_test_verify_nh_map_count("10:1::1", 1, map_list1);
+
+	/* Making it unusable should force a map rebuild */
+	dp_test_make_nh_unusable("dp1T1", "1::2");
+	dp_test_verify_nh_map_count("10::1", 1, map_list2);
+	/* The 2nd route used a different nh so is not modified */
+	dp_test_verify_nh_map_count("10:1::1", 1, map_list1);
+
+	dp_test_netlink_del_route(
+		"10::/64 "
+		"nh 1::2 int:dp1T1 "
+		"nh 2::1 int:dp2T1 backup");
+
+	dp_test_netlink_del_route(
+		"10:1::/64 "
+		"nh 1::3 int:dp1T1 "
+		"nh 2::3 int:dp2T1 backup");
+
+	/* Clean Up */
+
+	dp_test_nl_del_ip_addr_and_connected("dp1T1", "1::1/64");
+	dp_test_nl_del_ip_addr_and_connected("dp2T1", "2::2/64");
+	dp_test_nl_del_ip_addr_and_connected("dp3T1", "3::3/64");
+	dp_test_clear_path_unusable();
+
+} DP_END_TEST;
+
