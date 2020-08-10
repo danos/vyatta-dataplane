376198273636e234356689abfd508dec6c49949c
Ian Wilson <iw5120@intl.att.com>
2020-08-10 13:19:55 +0100

    npf: Add filters to session list cmd for src and dest addr and port
    VRVDR-52325

--- src/session/session_op.c
+++ src/session/session_op.c
@@ -20,6 +20,7 @@
 
 #include "if_var.h"
 #include "in6_var.h"
+#include "ip_addr.h"
 #include "compiler.h"
 #include "json_writer.h"
 #include "npf_shim.h"
@@ -50,9 +51,15 @@ enum sf_dir {
  * Session filter for list, show and clear commands
  */
 struct session_filter {
+	uint32_t	sf_addrids[SENTRY_LEN_IPV6];
+	uint32_t	sf_mask[SENTRY_LEN_IPV6];
+	uint		sf_addrids_depth;	/* Number of words to cmp */
+
 	bool		sf_ip;
 	bool		sf_ip6;
 	enum sf_dir	sf_dir;
+	uint8_t		sf_s_af;
+	uint8_t		sf_d_af;
 	uint16_t	sf_proto;
 	uint32_t	sf_ifindex;
 	uint64_t	sf_id;
@@ -89,6 +96,26 @@ static void __attribute__((format(printf, 2, 3))) cmd_err(FILE *f,
 			jsonw_destroy(&json);
 		}
 	}
+}
+
+/* Word offset into sf_addrids */
+static inline uint sess_addrids_offs(uint8_t af, bool src)
+{
+	if (src)
+		return 1;
+	return (af == AF_INET) ? 2 : 5;
+}
+
+static inline struct in6_addr *sess_addrids_saddr(uint8_t af, uint32_t *addrids)
+{
+	uint offs = sess_addrids_offs(af, true);
+	return (struct in6_addr *)&addrids[offs];
+}
+
+static inline struct in6_addr *sess_addrids_daddr(uint8_t af, uint32_t *addrids)
+{
+	uint offs = sess_addrids_offs(af, false);
+	return (struct in6_addr *)&addrids[offs];
 }
 
 /* Extract an uint from a string */
@@ -129,6 +156,51 @@ static ulong arg_to_ulong(const char *arg, int *error)
 	return val;
 }
 
+/*
+ * Parse address or prefix and mask.  Returns < 0 for failure.
+ */
+static int
+cmd_op_parse_addr_mask(const char *addr_str, struct in6_addr *addr,
+		       struct in6_addr *mask, uint8_t *af)
+{
+	char *p, *pp = NULL;
+	uint pfx_len = 0;
+	int rc = 0;
+
+	/* Separate address from prefix length (if present) */
+	p = strchr(addr_str, '/');
+	if (p) {
+		pp = p + 1;
+		*p = '\0';
+
+		pfx_len = arg_to_uint(pp, &rc);
+		if (rc < 0)
+			return rc;
+	}
+
+	/* IPv4 or IPv6? */
+	if (inet_pton(AF_INET, addr_str, addr) == 1) {
+		*af = AF_INET;
+
+		if (pfx_len == 0 || pfx_len > 32)
+			pfx_len = 32;
+
+		mask->s6_addr32[0] = prefixlen_to_mask(pfx_len);
+
+	} else if (inet_pton(AF_INET6, addr_str, addr) == 1) {
+		*af = AF_INET6;
+
+		if (pfx_len == 0 || pfx_len > 128)
+			pfx_len = 128;
+
+		in6_prefixlen2mask(mask, pfx_len);
+
+	} else
+		return -EINVAL;
+
+	return 0;
+}
+
 /* Pop next argument from list */
 static char *next_arg(int *argcp, char ***argvp)
 {
@@ -141,6 +213,117 @@ static char *next_arg(int *argcp, char ***argvp)
 	}
 
 	return arg;
+}
+
+/*
+ * Finish setting up the addrids filter
+ */
+static int
+cmd_op_finalize_addrids_fltr(FILE *f, struct session_filter *sf,
+			     uint16_t sport, uint16_t dport)
+{
+	uint offs, len;
+
+	if (sf->sf_s_af && sf->sf_d_af &&
+	    (sf->sf_s_af != sf->sf_d_af)) {
+		cmd_err(f,
+			"Mismatch between src-addr and dest-addr filters\n");
+		return -EINVAL;
+	}
+
+	/* Setup port filters */
+	if (sport || dport) {
+		sf->sf_addrids[0] = htons(sport) << 16 | htons(dport);
+
+		if (dport)
+			sf->sf_mask[0] = 0x0000FFFF;
+		if (sport)
+			sf->sf_mask[0] |= 0xFFFF0000;
+
+		/* Depth is the number of words to compare */
+		sf->sf_addrids_depth = 1;
+	}
+
+	/* Overwrite depth if src-addr specified */
+	if (sf->sf_s_af) {
+		offs = sess_addrids_offs(sf->sf_s_af, true);
+		len = (sf->sf_s_af == AF_INET) ? 1 : 4;
+
+		sf->sf_addrids_depth = offs + len;
+	}
+
+	/* Overwrite depth if dst-addr specified */
+	if (sf->sf_d_af) {
+		offs = sess_addrids_offs(sf->sf_d_af, false);
+		len = (sf->sf_d_af == AF_INET) ? 1 : 4;
+
+		sf->sf_addrids_depth = offs + len;
+	}
+	return 0;
+}
+
+static int
+cmd_op_parse_src_addr(FILE *f, int *argcp, char ***argvp,
+		      struct session_filter *sf)
+{
+	struct in6_addr addr, mask;
+	struct in6_addr *sf_addr, *sf_mask;
+	char *val;
+	int error;
+	uint8_t af;
+
+	val = next_arg(argcp, argvp);
+	if (!val)
+		goto error;
+
+	error = cmd_op_parse_addr_mask(val, &addr, &mask, &af);
+	if (error < 0)
+		goto error;
+
+	sf_addr = sess_addrids_saddr(af, sf->sf_addrids);
+	*sf_addr = addr;
+	sf->sf_s_af = af;
+
+	sf_mask = sess_addrids_saddr(af, sf->sf_mask);
+	*sf_mask = mask;
+
+	return 0;
+
+error:
+	cmd_err(f, "Error with src-addr filter params\n");
+	return -EINVAL;
+}
+
+static int
+cmd_op_parse_dst_addr(FILE *f, int *argcp, char ***argvp,
+		      struct session_filter *sf)
+{
+	struct in6_addr addr, mask;
+	struct in6_addr *sf_addr, *sf_mask;
+	char *val;
+	int error;
+	uint8_t af;
+
+	val = next_arg(argcp, argvp);
+	if (!val)
+		goto error;
+
+	error = cmd_op_parse_addr_mask(val, &addr, &mask, &af);
+	if (error < 0)
+		goto error;
+
+	sf_addr = sess_addrids_daddr(af, sf->sf_addrids);
+	*sf_addr = addr;
+	sf->sf_s_af = af;
+
+	sf_mask = sess_addrids_daddr(af, sf->sf_mask);
+	*sf_mask = mask;
+
+	return 0;
+
+error:
+	cmd_err(f, "Error with dest-addr filter params\n");
+	return -EINVAL;
 }
 
 static int cmd_op_parse_orderby(FILE *f, int *argcp, char ***argvp,
@@ -180,6 +363,7 @@ static int
 cmd_op_parse(FILE *f, int argc, char **argv, struct session_filter *sf,
 	     struct session_dump *sd)
 {
+	uint16_t sport = 0, dport = 0;
 	char *cmd, *val;
 	int error = 0;
 
@@ -234,6 +418,52 @@ cmd_op_parse(FILE *f, int argc, char **argv, struct session_filter *sf,
 
 			sf->sf_ifindex = ifp->if_index;
 
+		} else if (!strcmp(cmd, "src-addr")) {
+			/*
+			 * Source address filter
+			 */
+			error = cmd_op_parse_src_addr(f, &argc, &argv, sf);
+			if (error < 0)
+				return error;
+
+		} else if (!strcmp(cmd, "dst-addr")) {
+			/*
+			 * Destination address filter
+			 */
+			error = cmd_op_parse_dst_addr(f, &argc, &argv, sf);
+			if (error < 0)
+				return error;
+
+		} else if (!strcmp(cmd, "src-port")) {
+			/*
+			 * Source port filter
+			 */
+			uint tmp;
+			val = next_arg(&argc, &argv);
+			if (!val)
+				goto error_missing_param;
+
+			tmp = arg_to_uint(val, &error);
+			if (error < 0 || tmp > USHRT_MAX)
+				goto error_param_value;
+
+			sport = tmp;
+
+		} else if (!strcmp(cmd, "dst-port")) {
+			/*
+			 * Destination port filter
+			 */
+			uint tmp;
+			val = next_arg(&argc, &argv);
+			if (!val)
+				goto error_missing_param;
+
+			tmp = arg_to_uint(val, &error);
+			if (error < 0 || tmp > USHRT_MAX)
+				goto error_param_value;
+
+			dport = tmp;
+
 		} else if (!strcmp(cmd, "proto")) {
 			/*
 			 * Protocol filter
@@ -256,6 +486,11 @@ cmd_op_parse(FILE *f, int argc, char **argv, struct session_filter *sf,
 				return error;
 		}
 	}
+	cmd = NULL;
+
+	error = cmd_op_finalize_addrids_fltr(f, sf, sport, dport);
+	if (error < 0)
+		return error;
 
 	/* Default to both IP and IPv6 if neither is specified */
 	if (!sf->sf_ip && !sf->sf_ip6) {
@@ -281,6 +516,7 @@ static bool
 cmd_session_filter(const struct session *s, const struct session_filter *sf)
 {
 	const struct sentry *sen = rcu_dereference(s->se_sen);
+	uint i;
 
 	/* Session ID */
 	if (sf->sf_id && sf->sf_id != s->se_id)
@@ -311,6 +547,14 @@ cmd_session_filter(const struct session *s, const struct session_filter *sf)
 	/* Protocol */
 	if (sf->sf_proto && sf->sf_proto != sen->sen_protocol)
 		return false;
+
+	/* Port numbers, Source address, Dest address */
+	for (i = 0; i < sf->sf_addrids_depth; i++) {
+		uint32_t mask = sf->sf_mask[i];
+
+		if ((sf->sf_addrids[i] & mask) != (sen->sen_addrids[i] & mask))
+			return false;
+	}
 
 	return true;
 }
