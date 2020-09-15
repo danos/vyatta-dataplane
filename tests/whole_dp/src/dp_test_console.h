891e97b1fa025b4476631ebd79831478c659d7cf
Paul Atkins <paul.atkins@intl.att.com>
2020-09-15 09:49:01 +0100

    ut: remove unused code in dp_test_console.c
    VRVDR-52367

--- tests/whole_dp/src/dp_test_console.h
+++ tests/whole_dp/src/dp_test_console.h
@@ -11,8 +11,6 @@
 #ifndef _DP_TEST_CONSOLE_H_
 #define _DP_TEST_CONSOLE_H_
 
-char *dp_test_console_request_src(enum cont_src_en cont_src,
-				  const char *request, bool print);
 char *dp_test_console_request_w_err(const char *request, bool *err_ret,
 				    bool print);
 char *dp_test_console_request_w_err_src(enum cont_src_en cont_src,
@@ -26,8 +24,6 @@ void dp_test_console_request_pb(const char *req, int req_len,
 				zmsg_t **resp_msg,
 				bool print);
 void dp_test_console_request_reply(const char *cmd, bool print);
-void dp_test_console_request_reply_src(enum cont_src_en cont_src,
-				       const char *cmd, bool print);
 char *dp_test_console_set_endpoint(enum cont_src_en cont_src);
 
 #endif /* _DP_TEST_CONSOLE_H_ */
