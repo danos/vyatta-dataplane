c8edb57de848113b6ddb487dc0a2ca08511a777d
Ian Wilson <iw5120@intl.att.com>
2020-10-26 16:31:00 +0000

    ut: Add test for multicast forwarding in the dataplane
    VRVDR-53453. Enables multicast forwarding on two interfaces, adds a
    multicast route, injects one pkt to 224.0.0.1 and expects it to be
    forwarded on both output interfaces.

--- tests/whole_dp/src/dp_test_ip_multicast.c
+++ tests/whole_dp/src/dp_test_ip_multicast.c
@@ -106,3 +106,72 @@ DP_START_TEST(ip_mfwd_3, limited_broadcast)
 	dp_test_nl_del_ip_addr_and_connected("dp1T0", "1.1.1.1/24");
 	dp_test_nl_del_ip_addr_and_connected("dp2T1", "2.2.2.2/24");
 } DP_END_TEST;
+
+/*
+ * Multicast forwarding
+ *
+ * Enables multicast forwarding on two interfaces, added a multicast route,
+ * injects one pkt to 224.0.0.1 and expects it to be forwarded on both output
+ * interfaces.
+ */
+DP_DECL_TEST_CASE(ip_msuite, ip_mfwd_4, NULL, NULL);
+DP_START_TEST(ip_mfwd_4, dp_forwarding)
+{
+	const char *grp_dest = "224.0.1.1"; /* Not link local */
+	const char *grp_mac = "01:00:5e:00:01:01";
+	struct dp_test_expected *exp;
+	struct rte_mbuf *test_pak;
+	int len = 22;
+
+	/* Set up the interface addresses */
+	dp_test_nl_add_ip_addr_and_connected("dp1T0", "1.1.1.1/24");
+	dp_test_netlink_netconf_mcast("dp1T0", AF_INET, true);
+
+	dp_test_nl_add_ip_addr_and_connected("dp2T1", "2.2.2.2/24");
+	dp_test_netlink_netconf_mcast("dp2T1", AF_INET, true);
+
+	dp_test_nl_add_ip_addr_and_connected("dp2T2", "3.3.3.3/24");
+	dp_test_netlink_netconf_mcast("dp2T2", AF_INET, true);
+
+	/* Add multicast route */
+	dp_test_mroute_nl(RTM_NEWROUTE, "10.73.1.1", "dp1T0",
+			  "224.0.1.1/32 nh int:dp2T1 nh int:dp2T2");
+
+	/* Create multicast pak */
+	test_pak = dp_test_create_ipv4_pak("10.73.1.1", grp_dest, 1, &len);
+	dp_test_pktmbuf_eth_init(test_pak, dp_test_intf_name2mac_str("dp1T0"),
+				 DP_TEST_INTF_DEF_SRC_MAC, RTE_ETHER_TYPE_IPV4);
+
+	/* Create pak we expect to receive on the tx ring */
+	exp = dp_test_exp_create_m(test_pak, 2);
+	dp_test_exp_set_oif_name_m(exp, 0, "dp2T1");
+	dp_test_exp_set_oif_name_m(exp, 1, "dp2T2");
+
+	(void)dp_test_pktmbuf_eth_init(dp_test_exp_get_pak_m(exp, 0),
+				       grp_mac,
+				       dp_test_intf_name2mac_str("dp2T1"),
+				       RTE_ETHER_TYPE_IPV4);
+	dp_test_ipv4_decrement_ttl(dp_test_exp_get_pak_m(exp, 0));
+
+	(void)dp_test_pktmbuf_eth_init(dp_test_exp_get_pak_m(exp, 1),
+				       grp_mac,
+				       dp_test_intf_name2mac_str("dp2T2"),
+				       RTE_ETHER_TYPE_IPV4);
+	dp_test_ipv4_decrement_ttl(dp_test_exp_get_pak_m(exp, 1));
+
+	dp_test_pak_receive(test_pak, "dp1T0", exp);
+
+	/* Clean Up */
+	dp_test_mroute_nl(RTM_DELROUTE, "10.73.1.1", "dp1T0",
+			  "224.0.1.1/32 nh int:dp2T1 nh int:dp2T2");
+
+	dp_test_netlink_netconf_mcast("dp1T0", AF_INET, false);
+	dp_test_nl_del_ip_addr_and_connected("dp1T0", "1.1.1.1/24");
+
+	dp_test_netlink_netconf_mcast("dp2T1", AF_INET, false);
+	dp_test_nl_del_ip_addr_and_connected("dp2T1", "2.2.2.2/24");
+
+	dp_test_netlink_netconf_mcast("dp2T2", AF_INET, false);
+	dp_test_nl_del_ip_addr_and_connected("dp2T2", "3.3.3.3/24");
+
+} DP_END_TEST;
