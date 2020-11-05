28ce68a179d469f4d365dd45a499aa3f9bbb229f
Charles (Chas) Williams <ciwillia@vyatta.att-mail.com>
2020-11-04 20:55:49 -0500

    ptp: test edge cases in resolver
    VLANs and interface are ephermal. Attempt to test the code paths
    where this could happen.
    
    Bugfix: VRVDR-53538

--- tests/whole_dp/src/dp_test_ptp.c
+++ tests/whole_dp/src/dp_test_ptp.c
@@ -838,3 +838,150 @@ DP_START_TEST(ptp_cmds, resolver_two_uplinks)
 	bridge_vlan_set_free(allowed_vlans);
 
 } DP_END_TEST;
+
+DP_START_TEST(ptp_cmds, resolver_edge_cases)
+{
+	struct bridge_vlan_set *allowed_vlans = bridge_vlan_set_create();
+
+	bridge_vlan_set_add(allowed_vlans, 10);
+	bridge_vlan_set_add(allowed_vlans, 20);
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut clock create 0 "
+				"domain-number=0 "
+				"number-ports=2 "
+				"clock-identity=0:1:2:3:4:5:6:7 "
+				"priority1=128 "
+				"priority2=128 "
+				"slave-only=0 "
+				"two-step=0 "
+				"profile=default-profile");
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut port create 1 "
+				"clock-id=0 "
+				"underlying-interface=dpT10 "
+				"vlan-id=10 "
+				"log-min-delay-req-interval=1 "
+				"log-announce-interval=2 "
+				"announce-receipt-timeout=3 "
+				"log-min-pdelay-req-interval=1 "
+				"log-sync-interval=1 "
+				"ip=192.168.10.1 "
+				"mac=0:0:0:0:a:1 "
+				"dscp=0");
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut peer create "
+				"clock-id=0 "
+				"port-id=1 "
+				"type=master "
+				"ip=192.168.10.2");
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut port create 2 "
+				"clock-id=0 "
+				"underlying-interface=dpT11 "
+				"vlan-id=20 "
+				"log-min-delay-req-interval=1 "
+				"log-announce-interval=2 "
+				"announce-receipt-timeout=3 "
+				"log-min-pdelay-req-interval=1 "
+				"log-sync-interval=1 "
+				"ip=192.168.20.1 "
+				"mac=0:0:0:0:14:1 "
+				"dscp=0");
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut peer create "
+				"clock-id=0 "
+				"port-id=2 "
+				"type=slave "
+				"ip=192.168.20.2");
+
+	/* missing switch */
+	dp_test_check_state_show("ptp resolver trigger", "", true);
+
+	/* bridge configured */
+	dp_test_intf_bridge_create("sw0");
+	dp_test_intf_bridge_enable_vlan_filter("sw0");
+	dp_test_intf_bridge_add_port("sw0", "dpT10");
+	dp_test_intf_bridge_add_port("sw0", "dpT11");
+	dp_test_intf_bridge_port_set_vlans("sw0", "dpT10", 0,
+					   allowed_vlans, NULL);
+	dp_test_intf_bridge_port_set_vlans("sw0", "dpT11", 0,
+					   allowed_vlans, NULL);
+	dp_test_check_state_show("ptp resolver trigger", "", true);
+
+	/* routing interfaces */
+	dp_test_intf_vif_create("sw0.10", "sw0", 10);
+	dp_test_intf_vif_create("sw0.20", "sw0", 20);
+	dp_test_check_state_show("ptp resolver trigger", "", true);
+
+	dp_test_nl_add_ip_addr_and_connected("sw0.10", "192.168.10.1/24");
+	dp_test_nl_add_ip_addr_and_connected("sw0.20", "192.168.20.1/24");
+	dp_test_netlink_add_neigh("sw0.10", "192.168.10.2", "0:0:0:0:0:1");
+	dp_test_netlink_add_neigh("sw0.20", "192.168.20.2", "0:0:0:0:0:2");
+	dp_test_check_state_show("ptp resolver trigger", "", true);
+
+	/* admin down interfaces */
+	dp_test_netlink_set_interface_admin_status("dpT10", false);
+	dp_test_netlink_set_interface_admin_status("dpT11", false);
+	dp_test_check_state_show("ptp resolver trigger", "", true);
+
+	/* admin up interfaces */
+	dp_test_netlink_set_interface_admin_status("dpT10", true);
+	dp_test_netlink_set_interface_admin_status("dpT11", true);
+	dp_test_check_state_show("ptp resolver trigger", "", true);
+
+	dp_test_check_state_show("ptp resolver dump",
+			"{[{\n"
+			"            \"peer\": \"192.168.20.2\",\n"
+			"            \"installed\": true,\n"
+			"            \"port-id\": 2,\n"
+			"            \"mac\": \"0:0:0:0:0:2\",\n"
+			"            \"type\": \"slave\"\n"
+			"        },{\n"
+			"            \"peer\": \"192.168.10.2\",\n"
+			"            \"installed\": true,\n"
+			"            \"port-id\": 1,\n"
+			"            \"mac\": \"0:0:0:0:0:1\",\n"
+			"            \"type\": \"master\"\n"
+			"        }\n"
+			"    ]\n"
+			"    }\n", true);
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut peer delete "
+				"clock-id=0 port-id=1 "
+				"type=master "
+				"ip=192.168.10.2");
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut peer delete "
+				"clock-id=0 "
+				"port-id=2 "
+				"type=slave "
+				"ip=192.168.20.2");
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut port delete 1 clock-id=0");
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut port delete 2 clock-id=0");
+
+	dp_test_send_config_src(dp_test_cont_src_get(),
+				"ptp-ut clock delete 0");
+
+	dp_test_netlink_del_neigh("sw0.10", "192.168.10.2", "0:0:0:0:0:1");
+	dp_test_netlink_del_neigh("sw0.20", "192.168.20.2", "0:0:0:0:0:2");
+	dp_test_nl_del_ip_addr_and_connected("sw0.10", "192.168.10.1/24");
+	dp_test_nl_del_ip_addr_and_connected("sw0.20", "192.168.20.1/24");
+	dp_test_intf_bridge_remove_port("sw0", "dpT11");
+	dp_test_intf_bridge_remove_port("sw0", "dpT10");
+	dp_test_intf_vif_del("sw0.10", 10);
+	dp_test_intf_vif_del("sw0.20", 20);
+	dp_test_intf_bridge_del("sw0");
+	bridge_vlan_set_free(allowed_vlans);
+
+} DP_END_TEST;
