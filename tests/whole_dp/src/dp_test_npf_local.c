c2396d93bc3c8fdd1190cb708e768b394b7bae8b
Ian Wilson <iw5120@intl.att.com>
2020-11-05 20:05:08 +0000

    ut: Tests local ICMP pkts with egress ACLs and the originate fw
    VRVDR-53551

--- tests/whole_dp/src/dp_test_npf_local.c
+++ tests/whole_dp/src/dp_test_npf_local.c
@@ -1995,3 +1995,317 @@ DP_START_TEST(npf_v6nbr6, test)
 	dp_test_nl_del_ip_addr_and_connected("dp1T0", "2001:1:1::1/64");
 	dp_test_netlink_del_ip_address("dp1T0", router_ll_ip_subnet);
 } DP_END_TEST;
+
+/*
+ * ICMP originated packets with the originate firewall.
+ *
+ * Test generate ICMP message upon packet too big with don't fragment
+ * flag set (from dp_test_ip_icmp.c, DP_START_TEST(ip_icmp, df))
+ *
+ * Two ICMP packets are generated.  First one is allowed out via a PASS rule.
+ * Second one is dropped by a BLOCK rule.
+ *
+ * Exercises the ip_output code path.
+ */
+DP_DECL_TEST_CASE(npf_local, npf_icmp_orig1, NULL, NULL);
+DP_START_TEST(npf_icmp_orig1, test)
+{
+	struct dp_test_expected *exp;
+	struct rte_mbuf *icmp_pak;
+	struct rte_mbuf *test_pak;
+	const char *neigh3_mac_str = "aa:bb:cc:dd:ee:ff";
+	const char *neigh1_mac_str = "bb:aa:cc:ee:dd:ff";
+	struct iphdr *ip_inner;
+	struct icmphdr *icph;
+	struct iphdr *ip;
+	int len = 1472;
+	int icmplen;
+
+	/* Set up the interface addresses */
+	dp_test_nl_add_ip_addr_and_connected("dp1T1", "1.1.1.1/24");
+	dp_test_nl_add_ip_addr_and_connected("dp3T3", "2.2.2.2/24");
+
+	dp_test_netlink_set_interface_mtu("dp1T1", 1400);
+
+	/* Add the nh arp we want the packet to follow */
+	dp_test_netlink_add_neigh("dp3T3", "2.2.2.1", neigh3_mac_str);
+	dp_test_netlink_add_neigh("dp1T1", "1.1.1.2", neigh1_mac_str);
+
+	/* Add originate firewall rule */
+	struct dp_test_npf_rule_t rules[] = {
+		{
+			.rule     = "1",
+			.pass     = PASS,
+			.stateful = STATELESS,
+			.npf      = "proto-base=1",
+		},
+		RULE_DEF_BLOCK,
+		NULL_RULE
+	};
+
+	struct dp_test_npf_ruleset_t fw = {
+		.rstype = "originate",
+		.name   = "FW_ORIG",
+		.enable = 1,
+		.attach_point = "dp3T3",
+		.fwd    = FWD,
+		.dir    = "out",
+		.rules  = rules
+	};
+	dp_test_npf_fw_add(&fw, false);
+
+	/* Create pak to match the route added above */
+	test_pak = dp_test_create_ipv4_pak("2.2.2.1", "1.1.1.2", 1, &len);
+	ip = iphdr(test_pak);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_DF, 1);
+
+	(void)dp_test_pktmbuf_eth_init(test_pak,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       neigh3_mac_str, RTE_ETHER_TYPE_IPV4);
+
+	/*
+	 * Expected packet
+	 */
+	/* Create expected icmp packet  */
+	icmplen = sizeof(struct iphdr) + 576;
+	icmp_pak = dp_test_create_icmp_ipv4_pak("2.2.2.2", "2.2.2.1",
+						ICMP_DEST_UNREACH,
+						ICMP_FRAG_NEEDED,
+						DPT_ICMP_FRAG_DATA(1400),
+						1, &icmplen, iphdr(test_pak),
+						&ip, &icph);
+	(void)dp_test_pktmbuf_eth_init(icmp_pak,
+				       neigh3_mac_str,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       RTE_ETHER_TYPE_IPV4);
+
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TOS,
+				 IPTOS_PREC_INTERNETCONTROL);
+
+	ip_inner = (struct iphdr *)(icph + 1);
+
+	/* The TTL allowed to be changed from the original */
+	dp_test_set_pak_ip_field(ip_inner, DP_TEST_SET_TTL,
+				 DP_TEST_PAK_DEFAULT_TTL - 1);
+
+	exp = dp_test_exp_create(icmp_pak);
+	rte_pktmbuf_free(icmp_pak);
+
+	dp_test_exp_set_oif_name(exp, "dp3T3");
+
+	/* now send test pak and check we get expected back */
+	dp_test_pak_receive(test_pak, "dp3T3", exp);
+
+	/*
+	 * Repeat
+	 */
+	dp_test_npf_fw_del(&fw, false);
+	rules[0].pass = BLOCK;
+	dp_test_npf_fw_add(&fw, false);
+
+	/* Create pak to match the route added above */
+	test_pak = dp_test_create_ipv4_pak("2.2.2.1", "1.1.1.2", 1, &len);
+	ip = iphdr(test_pak);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_DF, 1);
+
+	(void)dp_test_pktmbuf_eth_init(test_pak,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       neigh3_mac_str, RTE_ETHER_TYPE_IPV4);
+
+	/*
+	 * Expected packet
+	 */
+	/* Create expected icmp packet  */
+	icmplen = sizeof(struct iphdr) + 576;
+	icmp_pak = dp_test_create_icmp_ipv4_pak("2.2.2.2", "2.2.2.1",
+						ICMP_DEST_UNREACH,
+						ICMP_FRAG_NEEDED,
+						DPT_ICMP_FRAG_DATA(1400),
+						1, &icmplen, iphdr(test_pak),
+						&ip, &icph);
+	(void)dp_test_pktmbuf_eth_init(icmp_pak,
+				       neigh3_mac_str,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       RTE_ETHER_TYPE_IPV4);
+
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TOS,
+				 IPTOS_PREC_INTERNETCONTROL);
+
+	ip_inner = (struct iphdr *)(icph + 1);
+
+	/* The TTL allowed to be changed from the original */
+	dp_test_set_pak_ip_field(ip_inner, DP_TEST_SET_TTL,
+				 DP_TEST_PAK_DEFAULT_TTL - 1);
+
+	exp = dp_test_exp_create(icmp_pak);
+	rte_pktmbuf_free(icmp_pak);
+
+	dp_test_exp_set_oif_name(exp, "dp3T3");
+	dp_test_exp_set_fwd_status(exp, DP_TEST_FWD_DROPPED);
+
+	/* now send test pak and check we get expected back */
+	dp_test_pak_receive(test_pak, "dp3T3", exp);
+
+
+	/* Clean Up */
+	dp_test_npf_fw_del(&fw, false);
+
+	dp_test_netlink_del_neigh("dp3T3", "2.2.2.1", neigh3_mac_str);
+	dp_test_netlink_del_neigh("dp1T1", "1.1.1.2", neigh1_mac_str);
+	dp_test_nl_del_ip_addr_and_connected("dp1T1", "1.1.1.1/24");
+	dp_test_nl_del_ip_addr_and_connected("dp3T3", "2.2.2.2/24");
+
+	dp_test_netlink_set_interface_mtu("dp1T1", 1500);
+} DP_END_TEST;
+
+/*
+ * ICMP originated packets with egress ACLs.
+ *
+ * Test generate ICMP message upon packet too big with don't fragment
+ * flag set (from dp_test_ip_icmp.c, DP_START_TEST(ip_icmp, df))
+ *
+ * Two ICMP packets are generated.  First one is allowed out via a PASS rule.
+ * Second one is dropped by a BLOCK rule.
+ *
+ * Exercises the ip_output code path.
+ */
+DP_DECL_TEST_CASE(npf_local, npf_icmp_orig2, NULL, NULL);
+DP_START_TEST(npf_icmp_orig2, test)
+{
+	struct dp_test_expected *exp;
+	struct rte_mbuf *icmp_pak;
+	struct rte_mbuf *test_pak;
+	const char *neigh3_mac_str = "aa:bb:cc:dd:ee:ff";
+	const char *neigh1_mac_str = "bb:aa:cc:ee:dd:ff";
+	struct iphdr *ip_inner;
+	struct icmphdr *icph;
+	struct iphdr *ip;
+	int len = 1472;
+	int icmplen;
+
+	/* Set up the interface addresses */
+	dp_test_nl_add_ip_addr_and_connected("dp1T1", "1.1.1.1/24");
+	dp_test_nl_add_ip_addr_and_connected("dp3T3", "2.2.2.2/24");
+
+	dp_test_netlink_set_interface_mtu("dp1T1", 1400);
+
+	/* Add the nh arp we want the packet to follow */
+	dp_test_netlink_add_neigh("dp3T3", "2.2.2.1", neigh3_mac_str);
+	dp_test_netlink_add_neigh("dp1T1", "1.1.1.2", neigh1_mac_str);
+
+	/* Add egress ACL */
+	dp_test_npf_cmd("npf-ut add acl:v4test 0 family=inet", false);
+	dp_test_npf_cmd("npf-ut add acl:v4test 10 "
+			"proto-base=1 "
+			"action=accept", false);
+	dp_test_npf_cmd("npf-ut attach interface:dpT33 acl-out acl:v4test",
+			false);
+	dp_test_npf_cmd("npf-ut commit", false);
+
+	/* Create pak to match the route added above */
+	test_pak = dp_test_create_ipv4_pak("2.2.2.1", "1.1.1.2", 1, &len);
+	ip = iphdr(test_pak);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_DF, 1);
+
+	(void)dp_test_pktmbuf_eth_init(test_pak,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       neigh3_mac_str, RTE_ETHER_TYPE_IPV4);
+
+	/*
+	 * Expected packet
+	 */
+	/* Create expected icmp packet  */
+	icmplen = sizeof(struct iphdr) + 576;
+	icmp_pak = dp_test_create_icmp_ipv4_pak("2.2.2.2", "2.2.2.1",
+						ICMP_DEST_UNREACH,
+						ICMP_FRAG_NEEDED,
+						DPT_ICMP_FRAG_DATA(1400),
+						1, &icmplen, iphdr(test_pak),
+						&ip, &icph);
+	(void)dp_test_pktmbuf_eth_init(icmp_pak,
+				       neigh3_mac_str,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       RTE_ETHER_TYPE_IPV4);
+
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TOS,
+				 IPTOS_PREC_INTERNETCONTROL);
+
+	ip_inner = (struct iphdr *)(icph + 1);
+
+	/* The TTL allowed to be changed from the original */
+	dp_test_set_pak_ip_field(ip_inner, DP_TEST_SET_TTL,
+				 DP_TEST_PAK_DEFAULT_TTL - 1);
+
+	exp = dp_test_exp_create(icmp_pak);
+	rte_pktmbuf_free(icmp_pak);
+
+	dp_test_exp_set_oif_name(exp, "dp3T3");
+
+	/* now send test pak and check we get expected back */
+	dp_test_pak_receive(test_pak, "dp3T3", exp);
+
+	/*
+	 * Repeat
+	 */
+	dp_test_npf_cmd("npf-ut add acl:v4test 10 "
+			"proto-base=1 "
+			"action=drop", false);
+	dp_test_npf_cmd("npf-ut commit", false);
+
+	/* Create pak to match the route added above */
+	test_pak = dp_test_create_ipv4_pak("2.2.2.1", "1.1.1.2", 1, &len);
+	ip = iphdr(test_pak);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_DF, 1);
+
+	(void)dp_test_pktmbuf_eth_init(test_pak,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       neigh3_mac_str, RTE_ETHER_TYPE_IPV4);
+
+	/*
+	 * Expected packet
+	 */
+	/* Create expected icmp packet  */
+	icmplen = sizeof(struct iphdr) + 576;
+	icmp_pak = dp_test_create_icmp_ipv4_pak("2.2.2.2", "2.2.2.1",
+						ICMP_DEST_UNREACH,
+						ICMP_FRAG_NEEDED,
+						DPT_ICMP_FRAG_DATA(1400),
+						1, &icmplen, iphdr(test_pak),
+						&ip, &icph);
+	(void)dp_test_pktmbuf_eth_init(icmp_pak,
+				       neigh3_mac_str,
+				       dp_test_intf_name2mac_str("dp3T3"),
+				       RTE_ETHER_TYPE_IPV4);
+
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TOS,
+				 IPTOS_PREC_INTERNETCONTROL);
+
+	ip_inner = (struct iphdr *)(icph + 1);
+
+	/* The TTL allowed to be changed from the original */
+	dp_test_set_pak_ip_field(ip_inner, DP_TEST_SET_TTL,
+				 DP_TEST_PAK_DEFAULT_TTL - 1);
+
+	exp = dp_test_exp_create(icmp_pak);
+	rte_pktmbuf_free(icmp_pak);
+
+	dp_test_exp_set_oif_name(exp, "dp3T3");
+	dp_test_exp_set_fwd_status(exp, DP_TEST_FWD_DROPPED);
+
+	/* now send test pak and check we get expected back */
+	dp_test_pak_receive(test_pak, "dp3T3", exp);
+
+
+	/* Clean Up */
+	dp_test_npf_cmd("npf-ut detach interface:dpT33 acl-out acl:v4test",
+			false);
+	dp_test_npf_cmd("npf-ut delete acl:v4test", false);
+	dp_test_npf_cmd("npf-ut commit", false);
+
+	dp_test_netlink_del_neigh("dp3T3", "2.2.2.1", neigh3_mac_str);
+	dp_test_netlink_del_neigh("dp1T1", "1.1.1.2", neigh1_mac_str);
+	dp_test_nl_del_ip_addr_and_connected("dp1T1", "1.1.1.1/24");
+	dp_test_nl_del_ip_addr_and_connected("dp3T3", "2.2.2.2/24");
+
+	dp_test_netlink_set_interface_mtu("dp1T1", 1500);
+} DP_END_TEST;
