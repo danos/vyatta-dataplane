c09c9fd4afa0febdb5ad6e8040c2ea20e6a19042
Paul Atkins <paul.atkins@intl.att.com>
2020-11-11 10:12:55 +0000

    ut:fal_plugin_framer: add parentheses around macro arguments
    VRVDR-53593

--- tests/whole_dp/src/fal_plugin_framer.c
+++ tests/whole_dp/src/fal_plugin_framer.c
@@ -12,35 +12,34 @@
 #include "fal_plugin_framer.h"
 
 #define DSA_GET_TAG_TYPE(_x) \
-	(((_x->tag[0]) & 0xc0) >> 6)
+	((((_x)->tag[0]) & 0xc0) >> 6)
 
 #define DSA_GET_DEVICE(_x) \
-	(_x->tag[0] & 0x1f)
-
+	((_x)->tag[0] & 0x1f)
 
 #define DSA_SET_DEVICE(_x, _y)			\
-	(_x->tag[0] |= (_y &  0x1f))
+	((_x)->tag[0] |= ((_y) &  0x1f))
 
 #define DSA_GET_PORT(_x)  \
-	((_x->tag[1] >> 3) & 0x1f)
+	(((_x)->tag[1] >> 3) & 0x1f)
 
 #define DSA_SET_PORT(_x, _y)			\
-	((_x->tag[1]) |=  (_y & 0x1f) << 3)
+	(((_x)->tag[1]) |=  ((_y) & 0x1f) << 3)
 
 #define DSA_GET_IS_TAGGED(_x) \
-	((_x->tag[0] >> 5) & 0x1)
+	(((_x)->tag[0] >> 5) & 0x1)
 
 #define DSA_SET_IS_TAGGED(_x)			\
-	((_x->tag[0]) |=  0x20)
+	(((_x)->tag[0]) |=  0x20)
 
 #define DSA_CLEAR_IS_TAGGED(_x)			\
-	((_x->tag[0]) &=  ~0x20)
+	(((_x)->tag[0]) &=  ~0x20)
 
 #define DSA_GET_CFI(_x)	 \
-	(_x->tag[1] & 0x1)
+	((_x)->tag[1] & 0x1)
 
 #define DSA_SET_CFI(_x)	 \
-	(_x->tag[1] |= 0x1)
+	((_x)->tag[1] |= 0x1)
 
 static inline struct rte_ether_hdr *ethhdr(struct rte_mbuf *m)
 {
