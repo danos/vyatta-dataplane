f5908e6bb21b8fbf24cbfa745c9e79d9c9006bc4
Paul Atkins <paul.atkins@intl.att.com>
2020-11-23 11:44:51 +0000

    ut:dp_test_lib_exp: use same parameter names in prototype and definition
    VRVDR-53593

--- tests/whole_dp/src/dp_test_lib_exp.h
+++ tests/whole_dp/src/dp_test_lib_exp.h
@@ -86,7 +86,7 @@ struct dp_test_expected {
 };
 
 void
-dp_test_exp_set_dont_care(struct dp_test_expected *exp, unsigned int check,
+dp_test_exp_set_dont_care(struct dp_test_expected *exp, unsigned int pak_i,
 			  uint8_t *start, uint32_t len);
 
 bool
