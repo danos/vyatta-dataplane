85fcd694cd0df1b7e75fd3308263d00798a62e97
Charles (Chas) Williams <ciwillia@vyatta.att-mail.com>
2020-12-28 16:44:48 -0500

    micro bfd: allow micro BFD packets when no address
    Even if we don't have an IPv6 address, we still need to be able to handle
    some layer 4 traffic. One example is micro BFD which exchanges packets
    on using a link-local MAC address. Since this is on a LAG member, the
    interface, being a LAG member, will never have an IPv6 address and must
    pass traffic to allow an IPv6 address to be negotiated for the LAG.
    
    Bugfix: VRVDR-46131

--- src/pipeline/nodes/l3_v6_no_address.c
+++ src/pipeline/nodes/l3_v6_no_address.c
@@ -8,9 +8,12 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <rte_branch_prediction.h>
+#include <rte_ether.h>
+#include <rte_mbuf.h>
 #include <stdbool.h>
 
 #include "compiler.h"
+#include "ether.h"
 #include "if_var.h"
 #include "pktmbuf_internal.h"
 
@@ -20,10 +23,25 @@
 #include "pl_nodes_common.h"
 #include "vrf_internal.h"
 
+static struct rte_ether_addr micro_bfd_dst = {
+	{ 0x01, 0x0, 0x5e, 0x90, 0x0, 0x01 } };
+
 ALWAYS_INLINE unsigned int
 ipv6_in_no_address_process(struct pl_packet *pkt __unused,
 			   void *context __unused)
 {
+	struct rte_mbuf *m = pkt->mbuf;
+	const struct rte_ether_hdr *eth = ethhdr(m);
+
+	/*
+	 * If this is a micro BFD packet, we need to handle it as
+	 * normal, even if we don't have an IPv6 address. It's likely
+	 * that this is a LAG member and will never have a valid IPv6
+	 * address.
+	 */
+	if (unlikely(rte_ether_addr_equal(&eth->d_addr, &micro_bfd_dst)))
+		return IPV6_IN_SPECIAL_PACKET;
+
 	/*
 	 * Special case of DHCP client, RFC2131 semantics
 	 *
@@ -42,6 +60,7 @@ PL_REGISTER_NODE(ipv6_in_no_address_node) = {
 	.num_next = IPV6_IN_NO_ADDRESS_NUM,
 	.next = {
 		[IPV6_IN_NO_ADDRESS_LOCAL] = "ipv6-local",
+		[IPV6_IN_SPECIAL_PACKET] = "ipv6-l4",
 	}
 };
 
