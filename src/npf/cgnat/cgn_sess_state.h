2807993292e3bac2ca4455f2a85ebdc46089ff71
Ian Wilson <ian.wilson@att.com>
2021-01-05 09:58:51 +0000

    Fixup copyright dates for 2021
    VRVDR-53801

--- src/npf/cgnat/cgn_sess_state.h
+++ src/npf/cgnat/cgn_sess_state.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, AT&T Intellectual Property.  All rights reserved.
+ * Copyright (c) 2019-2021, AT&T Intellectual Property.  All rights reserved.
  *
  * SPDX-License-Identifier: LGPL-2.1-only
  */
