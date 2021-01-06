6f43b2a96d6bcbd12dc76a3b8460bbecbc01fa21
Ian Wilson <ian.wilson@att.com>
2021-01-06 17:21:18 +0000

    Update soft_ticks using clock_gettime
    VRVDR-54042. clock_gettime is called every 10mS, with parameter
    CLOCK_REALTIME_COARSE in order to update an epoch timestamp.  This is
    used to provide a more accurate soft_ticks value.   We also provide an
    uptime in microsecs (soft_ticks_us) and a function to fetch the unix
    epoch in microsecs, unix_epoch_us.

--- src/soft_ticks.h
+++ src/soft_ticks.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2020, AT&T Intellectual Property.  All rights reserved.
+ * Copyright (c) 2019-2021, AT&T Intellectual Property.  All rights reserved.
  *
  * SPDX-License-Identifier: LGPL-2.1-only
  */
@@ -7,10 +7,29 @@
 #define _SOFT_TICKS_H_
 
 /*
- * Export of soft_ticks from controller.c
+ * Export of soft_ticks etc. from controller.c
+ *
+ * get_dp_uptime() may also be used to return the dataplane uptime in seconds.
+ * (This is also derived from soft_ticks)
  */
 
-/* Milliseconds.  Updated every 10 milliseconds. */
+/* Milliseconds since dataplane started. Updated every 10ms */
 extern volatile uint64_t soft_ticks;
+
+/* Microsecs since dataplane started. Updated every 10ms */
+extern uint64_t soft_ticks_us;
+
+/*
+ * Return soft_ticks value, with option to refresh it.  If refreshed then
+ * it will be accurate to 1ms, else it may be up to 10ms slow.
+ */
+uint64_t get_soft_ticks(bool refresh);
+
+/*
+ * Get unix epoch in microsecs. Updated every 10ms by default.  If 'refresh'
+ * is true then it is updated when this is called, and should be accurate to
+ * 1ms.
+ */
+uint64_t unix_epoch_us(bool refresh);
 
 #endif /* _SOFT_TICKS_H_ */
