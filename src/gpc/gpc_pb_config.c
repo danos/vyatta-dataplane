eea97550dae650ae9477ab711a1b9c6270e7ee14
Alan Dewar <alan.dewar@att.com>
2021-01-21 16:40:16 +0000

    GPC: add feature and table parsing
    Add the initial parsing of the GPCConfig and GPCTable protobufs.
    
    Part of VRVDR-46091

--- src/gpc/gpc_pb_config.c
+++ src/gpc/gpc_pb_config.c
@@ -11,19 +11,325 @@
 #include <vplane_log.h>
 #include <vplane_debug.h>
 #include <urcu/list.h>
+#include "gpc_pb.h"
+#include "gpc_util.h"
+#include "ip.h"
 #include "protobuf.h"
 #include "protobuf/GPCConfig.pb-c.h"
 #include "urcu.h"
+#include "util.h"
 
 /*
  * Local storage
  */
 static struct cds_list_head *gpc_feature_list;
 
-static int
-gpc_feature_parse(struct _GPCConfig *msg __unused)
+
+/*
+ * GPC table functions
+ */
+static void
+gpc_pb_table_free(struct rcu_head *head)
 {
+	struct gpc_pb_table *table;
+	uint32_t i;
+
+	table = caa_container_of(head, struct gpc_pb_table, table_rcu);
+
+	for (i = 0; i < table->n_table_names; i++)
+		free(table->table_names[i]);
+
+	free(table->ifname);
+	free(table);
+}
+
+static void
+gpc_pb_table_delete(struct gpc_pb_table *table)
+{
+	if (!table)
+		return;
+
+	cds_list_del(&table->table_list);
+	DP_DEBUG(GPC, DEBUG, GPC, "Freeing GPC table %p\n", table);
+
+	call_rcu(&table->table_rcu, gpc_pb_table_free);
+}
+
+static struct gpc_pb_table *
+gpc_pb_table_find(struct gpc_pb_feature *feature, const char *ifname,
+		  uint32_t location, uint32_t traffic_type)
+{
+	struct gpc_pb_table *table;
+
+	cds_list_for_each_entry(table, &feature->table_list, table_list) {
+		if (!strcmp(table->ifname, ifname) &&
+		    table->location == location &&
+		    table->traffic_type == traffic_type)
+			return table;
+	}
+	return NULL;
+}
+
+static int
+gpc_pb_table_add(struct gpc_pb_feature *feature, GPCTable *msg)
+{
+	struct gpc_pb_table *table;
+	uint32_t i;
+	int rv = 0;
+
+	table = calloc(1, sizeof(*table) +
+		       (sizeof(char *) * msg->n_table_names));
+	if (!table) {
+		RTE_LOG(ERR, GPC,
+			"Failed to allocate GPC table");
+		return -ENOMEM;
+	}
+
+	table->ifname = strdup(msg->ifname);
+	if (!table->ifname) {
+		rv = -ENOMEM;
+		goto error_path;
+	}
+	table->location = msg->location;
+
+	cds_list_add_tail(&table->table_list, &feature->table_list);
+	DP_DEBUG(GPC, DEBUG, GPC,
+		 "Allocated GPC table %p for %s/%s\n",
+		 table, table->ifname,
+		 gpc_get_table_location_str(table->location));
+
+	for (i = 0; i < msg->n_table_names; i++) {
+		table->table_names[i] = strdup(msg->table_names[i]);
+		if (!table->table_names[i]) {
+			rv = -ENOMEM;
+			goto error_path;
+		}
+		table->n_table_names = i;
+	}
+
+	CDS_INIT_LIST_HEAD(&table->rule_list);
+
+	return rv;
+
+ error_path:
+	RTE_LOG(ERR, GPC, "Failed to allocate memory for table\n");
+	gpc_pb_table_delete(table);
+
+	return rv;
+
+}
+
+static int
+gpc_pb_table_parse(struct gpc_pb_feature *feature, GPCTable *msg)
+{
+	struct gpc_pb_table *table;
+	enum gpc_config_action action;
+	int rv;
+
+	if (!msg) {
+		RTE_LOG(ERR, GPC,
+			"Failed to read GPCTable protobuf\n");
+		return -EPERM;
+	}
+	/*
+	 * Mandatory field checking.
+	 */
+	if (!msg->ifname || !msg->has_location || !msg->has_traffic_type) {
+		RTE_LOG(ERR, GPC,
+			"GPCTable protobuf missing mandatory field\n");
+		return -EPERM;
+	}
+
+	action = CREATE;
+	table = gpc_pb_table_find(feature, msg->ifname, msg->location,
+				  msg->traffic_type);
+	if (table) {
+		/*
+		 * If the table already exists, we delete it if the new
+		 * msg has no rules.  This is because if a GPC feature, e.g.
+		 * ACL has more than one table of the same type (IPv4/IPv6),
+		 * at the same location (ingress/egress/punt-path) and on the
+		 * same interface, the VCI code will have collapsed them into
+		 * a single table.  The multiple tables allow different ACL
+		 * use-cases to have separate tables in the CLI.
+		 */
+		DP_DEBUG(GPC, DEBUG, GPC,
+			 "Found existing table %s/%s, msg->rules: %p\n",
+			 msg->ifname,
+			 gpc_get_table_location_str(msg->location),
+			 msg->rules);
+		if (!msg->rules)
+			action = DELETE;
+		else
+			action = MODIFY;
+	}
+
+	switch (action) {
+	case CREATE:
+		rv = gpc_pb_table_add(feature, msg);
+		break;
+
+	case MODIFY:
+		/*
+		 * Modifies are "nuke-and-rebuild" to start with.
+		 * We will do in-place modifications at a later date.
+		 */
+		gpc_pb_table_delete(table);
+		rv  = gpc_pb_table_add(feature, msg);
+		break;
+
+	case DELETE:
+		gpc_pb_table_delete(table);
+		rv = 0;
+		break;
+
+	default:
+		break;
+	}
+	return rv;
+}
+
+/*
+ * GPC feature functions
+ */
+static void
+gpc_pb_feature_free(struct rcu_head *head)
+{
+	struct gpc_pb_feature *feature;
+
+	feature = caa_container_of(head, struct gpc_pb_feature, feature_rcu);
+	free(feature);
+}
+
+static void
+gpc_pb_feature_delete(struct gpc_pb_feature *feature)
+{
+	struct gpc_pb_table *table, *tmp_table;
+
+	if (!feature)
+		return;
+
+	cds_list_del(&feature->feature_list);
+	DP_DEBUG(GPC, DEBUG, GPC, "Freeing GPC feature %p\n", feature);
+
+	cds_list_for_each_entry_safe(table, tmp_table, &feature->table_list,
+				     table_list)
+		gpc_pb_table_delete(table);
+
+	call_rcu(&feature->feature_rcu, gpc_pb_feature_free);
+}
+
+static struct gpc_pb_feature *
+gpc_pb_feature_find(uint32_t type)
+{
+	struct gpc_pb_feature *feature;
+
+	if (!gpc_feature_list)
+		return NULL;
+
+	cds_list_for_each_entry(feature, gpc_feature_list, feature_list) {
+		if (feature->type == type)
+			return feature;
+	}
+	return NULL;
+}
+
+static int
+gpc_pb_feature_add(GPCConfig *msg)
+{
+	struct gpc_pb_feature *feature;
+	uint32_t i;
+	int32_t rv;
+
+	feature = calloc(1, sizeof(*feature));
+	if (!feature) {
+		RTE_LOG(ERR, GPC,
+			"Failed to allocate GPC feature");
+		return -ENOMEM;
+	}
+
+	feature->type = msg->feature_type;
+
+	DP_DEBUG(GPC, DEBUG, GPC,
+		 "Allocated GPC feature %p for %s\n", feature,
+		 gpc_get_feature_type_str(feature->type));
+
+	CDS_INIT_LIST_HEAD(&feature->table_list);
+	CDS_INIT_LIST_HEAD(&feature->counter_list);
+	cds_list_add_tail(&feature->feature_list, gpc_feature_list);
+
+	/* Parse the rest of config message */
+	for (i = 0; i < msg->n_tables; i++) {
+		rv = gpc_pb_table_parse(feature, msg->tables[i]);
+		if (rv)
+			goto error_path;
+	}
+
 	return 0;
+
+ error_path:
+	RTE_LOG(ERR, GPC, "Failed to add GPC feature, type: %s: %d\n",
+		gpc_get_feature_type_str(feature->type), rv);
+	gpc_pb_feature_delete(feature);
+	return rv;
+}
+
+static int
+gpc_pb_feature_parse(GPCConfig *msg)
+{
+	struct gpc_pb_feature *feature;
+	enum gpc_config_action action;
+	int rv;
+
+	if (!msg) {
+		RTE_LOG(ERR, GPC,
+			"Failed to read GPCConfig protobuf\n");
+		return -EPERM;
+	}
+
+	if (!msg->has_feature_type) {
+		RTE_LOG(ERR, GPC,
+			"GPCConfig protobuf missing mandatory field\n");
+		return -EPERM;
+	}
+
+	action = CREATE;
+	feature = gpc_pb_feature_find(msg->feature_type);
+	if (feature) {
+		/*
+		 * If the feature already exists we delete it if the new
+		 * config-msg has no tables.
+		 */
+		if (!msg->n_tables)
+			action = DELETE;
+		else
+			action = MODIFY;
+	}
+
+	switch (action) {
+	case CREATE:
+		rv = gpc_pb_feature_add(msg);
+		break;
+
+	case MODIFY:
+		/*
+		 * Modifies are "nuke-and-rebuild" to start with.
+		 * We will do in-place modifications at a later date.
+		 */
+		gpc_pb_feature_delete(feature);
+		rv = gpc_pb_feature_add(msg);
+		break;
+
+	case DELETE:
+		gpc_pb_feature_delete(feature);
+		rv = 0;
+		break;
+
+	default:
+		rv = -EINVAL;
+		break;
+	}
+	return rv;
 }
 
 static int
@@ -46,7 +352,7 @@ gpc_config(struct pb_msg *msg)
 		CDS_INIT_LIST_HEAD(gpc_feature_list);
 	}
 
-	rv = gpc_feature_parse(config_msg);
+	rv = gpc_pb_feature_parse(config_msg);
 
 	gpcconfig__free_unpacked(config_msg, NULL);
 	return rv;
