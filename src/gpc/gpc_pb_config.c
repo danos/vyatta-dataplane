5627c375e4ef80b644832efee74dd34dfaaf5d15
Alan Dewar <alan.dewar@att.com>
2021-01-22 10:36:58 +0000

    GPC: give gpc_config.c a better name
    Part of VRVDR-46091

--- src/gpc/gpc_pb_config.c
+++ src/gpc/gpc_pb_config.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2020, AT&T Intellectual Property.
+ * Copyright (c) 2020-2021, AT&T Intellectual Property.
  * All rights reserved.
  *
  * SPDX-License-Identifier: LGPL-2.1-only
