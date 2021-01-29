fd1a785670591796b690038176c155c726e137cb
Simon Barber <sbarber@mail.eng.vyatta.net>
2021-01-29 14:03:56 +0000

    Revert "GPC: add more utility functions"
    This reverts commit 4e261a43cc5e69e56062a7894505369da3fa1179.
    
    VRVDR-54271

--- src/gpc/gpc_util.c
+++ src/gpc/gpc_util.c
@@ -17,59 +17,10 @@
  * GPC enum to string mapping definitions and functions
  */
 
-const char *gpc_action_type_str[] = {
-	[RULE_ACTION__ACTION_VALUE__NOT_SET] = "not-set",
-	[RULE_ACTION__ACTION_VALUE_DECISION] = "decision",
-	[RULE_ACTION__ACTION_VALUE_DESIGNATION] = "set-designation",
-	[RULE_ACTION__ACTION_VALUE_COLOUR] = "set-colour",
-	[RULE_ACTION__ACTION_VALUE_POLICER] = "policer",
-};
-
-const char *gpc_policer_awareness_str[] = {
-	[POLICER_AWARENESS__AWARENESS_UNKNOWN] = "unknown",
-	[POLICER_AWARENESS__COLOUR_AWARE] = "aware",
-	[POLICER_AWARENESS__COLOUR_UNAWARE] = "unaware",
-};
-
-const char *gpc_cntr_format_str[] = {
-	[GPCCOUNTER__COUNTER_FORMAT__FORMAT_UNKNOWN] = "format-unknown",
-	[GPCCOUNTER__COUNTER_FORMAT__PACKETS_ONLY] = "packets",
-	[GPCCOUNTER__COUNTER_FORMAT__PACKETS_AND_L2_L3_BYTES] =
-		"packets-and-l2-and-l3-bytes",
-};
-
 const char *gpc_feature_type_str[] = {
 	[GPCCONFIG__FEATURE_TYPE__FEATURE_UNKNOWN] = "unknown",
 	[GPCCONFIG__FEATURE_TYPE__QOS] = "qos",
 	[GPCCONFIG__FEATURE_TYPE__ACL] = "acl",
-};
-
-const char *gpc_match_type_str[] = {
-	[RULE_MATCH__MATCH_VALUE__NOT_SET] = "not-set",
-	[RULE_MATCH__MATCH_VALUE_SRC_IP] = "src-ip",
-	[RULE_MATCH__MATCH_VALUE_DEST_IP] = "dst-ip",
-	[RULE_MATCH__MATCH_VALUE_SRC_PORT] = "src-port",
-	[RULE_MATCH__MATCH_VALUE_DEST_PORT] = "dst-port",
-	[RULE_MATCH__MATCH_VALUE_FRAGMENT] = "fragment",
-	[RULE_MATCH__MATCH_VALUE_DSCP] = "dscp",
-	[RULE_MATCH__MATCH_VALUE_TTL] = "ttl",
-	[RULE_MATCH__MATCH_VALUE_ICMPV4] = "icmpv4",
-	[RULE_MATCH__MATCH_VALUE_ICMPV6] = "icmpv6",
-	[RULE_MATCH__MATCH_VALUE_ICMPV6_CLASS] = "icmpv6-class",
-	[RULE_MATCH__MATCH_VALUE_PROTO_BASE] = "proto-base",
-	[RULE_MATCH__MATCH_VALUE_PROTO_FINAL] = "proto-final",
-};
-
-const char *gpc_pkt_colour_str[] = {
-	[RULE_ACTION__COLOUR_VALUE__GREEN] = "green",
-	[RULE_ACTION__COLOUR_VALUE__YELLOW] = "yellow",
-	[RULE_ACTION__COLOUR_VALUE__RED] = "red",
-};
-
-const char *gpc_pkt_decision_str[] = {
-	[RULE_ACTION__PACKET_DECISION__DECISION_UNKNOWN] = "unknown",
-	[RULE_ACTION__PACKET_DECISION__PASS] = "pass",
-	[RULE_ACTION__PACKET_DECISION__DROP] = "drop",
 };
 
 const char *gpc_table_location_str[] = {
@@ -100,52 +51,10 @@ gpc_get_str(uint32_t index, uint32_t size, const char *str_array[])
 }
 
 const char *
-gpc_get_action_type_str(uint32_t action)
-{
-	return gpc_get_str(action, ARRAY_SIZE(gpc_action_type_str),
-			   gpc_action_type_str);
-}
-
-const char *
-gpc_get_cntr_format_str(uint32_t format)
-{
-	return gpc_get_str(format, ARRAY_SIZE(gpc_cntr_format_str),
-			   gpc_cntr_format_str);
-}
-
-const char *
 gpc_get_feature_type_str(uint32_t type)
 {
 	return gpc_get_str(type, ARRAY_SIZE(gpc_feature_type_str),
 			   gpc_feature_type_str);
-}
-
-const char *
-gpc_get_match_type_str(uint32_t match)
-{
-	return gpc_get_str(match, ARRAY_SIZE(gpc_match_type_str),
-			   gpc_match_type_str);
-}
-
-const char *
-gpc_get_pkt_colour_str(uint32_t colour)
-{
-	return gpc_get_str(colour, ARRAY_SIZE(gpc_pkt_colour_str),
-			   gpc_pkt_colour_str);
-}
-
-const char *
-gpc_get_pkt_decision_str(uint32_t decision)
-{
-	return gpc_get_str(decision, ARRAY_SIZE(gpc_pkt_decision_str),
-			   gpc_pkt_decision_str);
-}
-
-const char *
-gpc_get_policer_awareness_str(uint32_t awareness)
-{
-	return gpc_get_str(awareness, ARRAY_SIZE(gpc_policer_awareness_str),
-			   gpc_policer_awareness_str);
 }
 
 const char *
@@ -162,35 +71,3 @@ gpc_get_traffic_type_str(uint32_t traffic_type)
 			   gpc_traffic_type_str);
 }
 
-static uint32_t
-gpc_get_value(const char *str, uint32_t size, const char *str_array[])
-{
-	uint32_t i;
-
-	for (i = 0; i < size; i++) {
-		if (!strcmp(str_array[i], str))
-			return i;
-	}
-	return 0;
-}
-
-uint32_t
-gpc_feature_str_to_type(const char *str)
-{
-	return gpc_get_value(str, ARRAY_SIZE(gpc_feature_type_str),
-			     gpc_feature_type_str);
-}
-
-uint32_t
-gpc_table_location_str_to_value(const char *str)
-{
-	return gpc_get_value(str, ARRAY_SIZE(gpc_table_location_str),
-			     gpc_table_location_str);
-}
-
-uint32_t
-gpc_traffic_type_str_to_value(const char *str)
-{
-	return gpc_get_value(str, ARRAY_SIZE(gpc_traffic_type_str),
-			     gpc_traffic_type_str);
-}
