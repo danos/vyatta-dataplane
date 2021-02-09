46686b20c5edd598b1fb275504930bbb2a74aedf
Ian Wilson <ian.wilson@att.com>
2021-02-09 16:31:52 +0000

    ut: Add test sip_nat40 to dp_test_npf_alg_sip_nat.c
    Tests SIP data set #4 with DNAT configured.
    VRVDR-54421

--- tests/whole_dp/src/dp_test_npf_alg_sip_nat.c
+++ tests/whole_dp/src/dp_test_npf_alg_sip_nat.c
@@ -23,6 +23,9 @@ static void dpt_alg_sipd2_teardown(void);
 static void dpt_alg_sipd3_setup(void);
 static void dpt_alg_sipd3_teardown(void);
 
+static void dpt_alg_sipd4_setup(void);
+static void dpt_alg_sipd4_teardown(void);
+
 DP_DECL_TEST_SUITE(sip_nat);
 
 /*
@@ -619,6 +622,78 @@ DP_START_TEST(sip_nat30, test)
 } DP_END_TEST; /* sip_nat30 */
 
 
+/*
+ * sip_nat40.  Data set #4. DNAT.
+ */
+DP_DECL_TEST_CASE(sip_nat, sip_nat40, dpt_alg_sipd4_setup,
+		  dpt_alg_sipd4_teardown);
+DP_START_TEST(sip_nat40, test)
+{
+	const char *desc, *pre_pload, *pst_pload;
+	uint hdr_clen, body_clen;
+	bool forw, rv;
+	uint i;
+
+	assert(ARRAY_SIZE(sipd4_pre_dnat) == ARRAY_SIZE(sipd4_post_dnat));
+	assert(ARRAY_SIZE(sipd4_pre_dnat) == ARRAY_SIZE(sipd4_dir));
+
+	struct dp_test_npf_nat_rule_t dnat = {
+		.desc		= "DNAT rule",
+		.rule		= "1",
+		.ifname		= "dp1T1",
+		.proto		= IPPROTO_UDP,
+		.map		= "dynamic",
+		.from_addr	= NULL,
+		.from_port	= NULL,
+		.to_addr	= "77.1.1.1",
+		.to_port	= NULL,
+		.trans_addr	= "16.33.0.200",
+		.trans_port	= NULL
+	};
+	dp_test_npf_dnat_add(&dnat, true);
+
+	for (i = 0; i < ARRAY_SIZE(sipd4_dir); i++) {
+		pre_pload = sipd4_pre_dnat[i];
+		pst_pload = sipd4_post_dnat[i];
+		forw = (sipd4_dir[i] == SIP_FORW);
+		desc = sipd_descr(i, forw, pst_pload);
+
+		/* Check content-length value matches actual content-length */
+		rv = sipd_check_content_length(pre_pload, &hdr_clen,
+					       &body_clen);
+		dp_test_fail_unless(rv, "[%s] hdr=%u, body=%u",
+				    desc, hdr_clen, body_clen);
+
+		rv = sipd_check_content_length(pst_pload, &hdr_clen,
+					       &body_clen);
+		dp_test_fail_unless(rv, "[%s] hdr=%u, body=%u",
+				    desc, hdr_clen, body_clen);
+
+		if (forw) {
+			dpt_udp_pl("dp1T1", "7c:69:f6:4a:3a:50",
+				   "18.33.0.200", 60673, "77.1.1.1", 5060,
+				   "18.33.0.200", 60673, "16.33.0.200", 5060,
+				   "70:6e:6d:88:55:80", "dp1T0",
+				   DP_TEST_FWD_FORWARDED,
+				   pre_pload, strlen(pre_pload),
+				   pst_pload, strlen(pst_pload), desc);
+		} else {
+			dpt_udp_pl("dp1T0", "70:6e:6d:88:55:80",
+				   "16.33.0.200", 5060, "18.33.0.200", 60673,
+				   "77.1.1.1", 5060, "18.33.0.200", 60673,
+				   "7c:69:f6:4a:3a:50", "dp1T1",
+				   DP_TEST_FWD_FORWARDED,
+				   pre_pload, strlen(pre_pload),
+				   pst_pload, strlen(pst_pload), desc);
+		}
+
+	}
+
+	dp_test_npf_dnat_del(dnat.ifname, dnat.rule, true);
+
+} DP_END_TEST; /* sip_nat40 */
+
+
 static void dpt_alg_sipd1_setup(void)
 {
 	/* Setup interfaces and neighbours */
@@ -700,4 +775,30 @@ static void dpt_alg_sipd3_teardown(void)
 
 	dp_test_nl_del_ip_addr_and_connected("dp1T0", "8.19.19.1/24");
 	dp_test_nl_del_ip_addr_and_connected("dp2T1", "50.60.70.1/24");
+}
+
+static void dpt_alg_sipd4_setup(void)
+{
+	/* Setup interfaces and neighbours */
+	dp_test_nl_add_ip_addr_and_connected("dp1T0", "16.33.0.220/24");
+	dp_test_nl_add_ip_addr_and_connected("dp1T1", "18.33.0.220/24");
+
+	dp_test_netlink_add_neigh("dp1T0", "16.33.0.200",
+				  "70:6e:6d:88:55:80");
+	dp_test_netlink_add_neigh("dp1T1", "18.33.0.200",
+				  "7c:69:f6:4a:3a:50");
+}
+
+static void dpt_alg_sipd4_teardown(void)
+{
+	dp_test_npf_cleanup();
+
+	dp_test_netlink_del_neigh("dp1T0", "16.33.0.200",
+				  "70:6e:6d:88:55:80");
+	dp_test_netlink_del_neigh("dp1T1", "18.33.0.200",
+				  "7c:69:f6:4a:3a:50");
+
+	dp_test_nl_del_ip_addr_and_connected("dp1T0", "16.33.0.220/24");
+	dp_test_nl_del_ip_addr_and_connected("dp1T1", "18.33.0.220/24");
+
 }
