ef95f3188a2b90d9d5731661386adf1ab90db64e
Ian Wilson <ian.wilson@att.com>
2021-02-09 09:35:13 +0000

    ut: Add test sip_nat11 to dp_test_npf_alg_sip_nat.c
    Tests SIP data set #1 with SNAT configured.
    VRVDR-54421

--- tests/whole_dp/src/dp_test_npf_alg_sip_data1.c
+++ tests/whole_dp/src/dp_test_npf_alg_sip_data1.c
@@ -158,3 +158,206 @@ const char *sipd1[SIPD1_SZ] = {
 	"Content-Length: 0\r\n"
 	"\r\n",
 };
+
+/*
+ * SNAT
+ */
+const char *sipd1_pre_snat[SIPD1_SZ] = {
+	/*
+	 * 0. INVITE. Forward (inside)
+	 */
+	"INVITE sip:B.Boss@22.22.22.2 SIP/2.0\r\n"
+	"Via: SIP/2.0/UDP 1.1.1.2:5060;branch=z9hG4bKfw19b\r\n"
+	"Record-Route: <sip:1.1.1.2;lr>\r\n"
+	"From: A. Workman <sip:workman@1.1.1.2>;tag=76341\r\n"
+	"To: B.Boss <sip:B.Boss@work.co.uk>\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1 INVITE\r\n"
+	"Contact: <sip:workman@1.1.1.2>\r\n"
+	"Content-Type: application/sdp\r\n"
+	"P-asserted-identity: \"Workman\" <sip:workman@1.1.1.2>\r\n"
+	"Max-forwards: 70\r\n"
+	"Subject: About That Power Outage...\r\n"
+	"Content-Length:   139\r\n"
+	"\r\n"
+	"v=0\r\n"
+	"o=Workman 2890844526 2890844526 IN IP4 1.1.1.2\r\n"
+	"s=Phone Call\r\n"
+	"c=IN IP4 1.1.1.2\r\n"
+	"t=0 0\r\n"
+	"m=audio 10000 RTP/AVP 0\r\n"
+	"a=rtpmap:0 PCMU/8000\r\n",
+
+	/*
+	 * 1. 180 Ringing. Back (outside)
+	 */
+	"SIP/2.0 180 Ringing\r\n"
+	"Via: SIP/2.0/UDP 30.30.30.2:1024;branch=z9hG4bKfw19b\r\n"
+	"Record-Route: <sip:22.22.22.2;lr>\r\n"
+	"From: A. Workman <sip:workman@30.30.30.2>;tag=76341\r\n"
+	"To: B.Boss <sip:boss@22.22.22.2>;tag=a53e42\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1 INVITE\r\n"
+	"Contact: <sip:boss@22.22.22.2>\r\n"
+	"Content-Length: 0\r\n"
+	"\r\n",
+
+	/*
+	 * 2. 200 OK. Back
+	 */
+	"SIP/2.0 200 OK\r\n"
+	"Via: SIP/2.0/UDP 30.30.30.2:1024;branch=z9hG4bKfw19b\r\n"
+	"From: A. Workman <sip:workman@30.30.30.2>;tag=76341\r\n"
+	"To: B.Boss <sip:boss@work.co.uk>;tag=a53e42\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1 INVITE\r\n"
+	"Contact: <sip:boss@22.22.22.2>\r\n"
+	"Content-Type: application/sdp\r\n"
+	"Content-Length:   144\r\n"
+	"\r\n"
+	"v=0\r\n"
+	"o=B.Boss 2890844528 2890844528 IN IP4 22.22.22.2\r\n"
+	"s=Phone Call\r\n"
+	"c=IN IP4 22.22.22.2\r\n"
+	"t=0 0\r\n"
+	"m=audio 60000 RTP/AVP 0\r\n"
+	"a=rtpmap:0 PCMU/8000\r\n",
+
+	/*
+	 * 3. ACK. Forward
+	 */
+	"ACK sip:boss@22.22.22.2 SIP/2.0\r\n"
+	"Via: SIP/2.0/UDP 1.1.1.2:5060;branch=z9hG4bK321g\r\n"
+	"From: A. Workman <sip:workman@home.com>;tag=76341\r\n"
+	"To: B.Boss <sip:boss@work.co.uk>;tag=a53e42\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1 ACK\r\n"
+	"Max-forwards: 70\r\n"
+	"Content-Length: 0\r\n"
+	"\r\n",
+
+	/*
+	 * 4. BYE. Back
+	 */
+	"BYE sip:workman@30.30.30.2 SIP/2.0\r\n"
+	"Via: SIP/2.0/UDP 22.22.22.2:5060;branch=z9hG4bK392kf\r\n"
+	"From: B.Boss <sip:boss@22.22.22.2>;tag=a53e42\r\n"
+	"To: A. Workman <sip:workman@30.30.30.2>;tag=76341\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1392 BYE\r\n"
+	"Max-forwards: 70\r\n"
+	"Content-Length: 0\r\n"
+	"\r\n",
+	/*
+	 * 5. Forward
+	 */
+	"SIP/2.0 200 OK\r\n"
+	"Via: SIP/2.0/UDP 22.22.22.2:5060;branch=z9hG4bK392kf\r\n"
+	"From: B.Boss <sip:boss@22.22.22.2>;tag=a53e42\r\n"
+	"To: A. Workman <sip:workman@1.1.1.2>;tag=76341\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1392 BYE\r\n"
+	"Content-Length: 0\r\n"
+	"\r\n",
+};
+
+const char *sipd1_post_snat[SIPD1_SZ] = {
+	/*
+	 * 0. INVITE. Forward (outside)
+	 */
+	"INVITE sip:B.Boss@22.22.22.2 SIP/2.0\r\n"
+	"Via: SIP/2.0/UDP 30.30.30.2:1024;branch=z9hG4bKfw19b\r\n"
+	"Record-Route: <sip:30.30.30.2;lr>\r\n"
+	"From: A. Workman <sip:workman@30.30.30.2>;tag=76341\r\n"
+	"To: B.Boss <sip:B.Boss@work.co.uk>\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1 INVITE\r\n"
+	"Contact: <sip:workman@30.30.30.2>\r\n"
+	"Content-Type: application/sdp\r\n"
+	"P-asserted-identity: \"Workman\" <sip:workman@30.30.30.2>\r\n"
+	"Max-forwards: 70\r\n"
+	"Subject: About That Power Outage...\r\n"
+	"Content-Length:   144\r\n"
+	"\r\n"
+	"v=0\r\n"
+	"o=Workman 2890844526 2890844526 IN IP4 30.30.30.2\r\n"
+	"s=Phone Call\r\n"
+	"c=IN IP4 30.30.30.2\r\n"
+	"t=0 0\r\n"
+	"m=audio 1026 RTP/AVP 0\r\n"
+	"a=rtpmap:0 PCMU/8000\r\n",
+
+	/*
+	 * 1. 180 Ringing. Back (inside)
+	 */
+	"SIP/2.0 180 Ringing\r\n"
+	"Via: SIP/2.0/UDP 1.1.1.2:5060;branch=z9hG4bKfw19b\r\n"
+	"Record-Route: <sip:22.22.22.2;lr>\r\n"
+	"From: A. Workman <sip:workman@1.1.1.2>;tag=76341\r\n"
+	"To: B.Boss <sip:boss@22.22.22.2>;tag=a53e42\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1 INVITE\r\n"
+	"Contact: <sip:boss@22.22.22.2>\r\n"
+	"Content-Length: 0\r\n"
+	"\r\n",
+
+	/*
+	 * 2. 200 OK. Back
+	 */
+	"SIP/2.0 200 OK\r\n"
+	"Via: SIP/2.0/UDP 1.1.1.2:5060;branch=z9hG4bKfw19b\r\n"
+	"From: A. Workman <sip:workman@1.1.1.2>;tag=76341\r\n"
+	"To: B.Boss <sip:boss@work.co.uk>;tag=a53e42\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1 INVITE\r\n"
+	"Contact: <sip:boss@22.22.22.2>\r\n"
+	"Content-Type: application/sdp\r\n"
+	"Content-Length:   144\r\n"
+	"\r\n"
+	"v=0\r\n"
+	"o=B.Boss 2890844528 2890844528 IN IP4 22.22.22.2\r\n"
+	"s=Phone Call\r\n"
+	"c=IN IP4 22.22.22.2\r\n"
+	"t=0 0\r\n"
+	"m=audio 60000 RTP/AVP 0\r\n"
+	"a=rtpmap:0 PCMU/8000\r\n",
+
+	/*
+	 * 3. ACK. Forward
+	 */
+	"ACK sip:boss@22.22.22.2 SIP/2.0\r\n"
+	"Via: SIP/2.0/UDP 30.30.30.2:1024;branch=z9hG4bK321g\r\n"
+	"From: A. Workman <sip:workman@home.com>;tag=76341\r\n"
+	"To: B.Boss <sip:boss@work.co.uk>;tag=a53e42\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1 ACK\r\n"
+	"Max-forwards: 70\r\n"
+	"Content-Length: 0\r\n"
+	"\r\n",
+
+	/*
+	 * 4. BYE. Back
+	 */
+	"BYE sip:workman@1.1.1.2 SIP/2.0\r\n"
+	"Via: SIP/2.0/UDP 22.22.22.2:5060;branch=z9hG4bK392kf\r\n"
+	"From: B.Boss <sip:boss@22.22.22.2>;tag=a53e42\r\n"
+	"To: A. Workman <sip:workman@1.1.1.2>;tag=76341\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1392 BYE\r\n"
+	"Max-forwards: 70\r\n"
+	"Content-Length: 0\r\n"
+	"\r\n",
+
+	/*
+	 * 5. 200 OK. Forward
+	 */
+	"SIP/2.0 200 OK\r\n"
+	"Via: SIP/2.0/UDP 22.22.22.2:5060;branch=z9hG4bK392kf\r\n"
+	"From: B.Boss <sip:boss@22.22.22.2>;tag=a53e42\r\n"
+	"To: A. Workman <sip:workman@30.30.30.2>;tag=76341\r\n"
+	"Call-ID: j2qu348ek2328ws\r\n"
+	"CSeq: 1392 BYE\r\n"
+	"Content-Length: 0\r\n"
+	"\r\n",
+};
+
--- tests/whole_dp/src/dp_test_npf_alg_sip_nat.c
+++ tests/whole_dp/src/dp_test_npf_alg_sip_nat.c
@@ -66,6 +66,112 @@ DP_START_TEST(sip_nat10, test)
 
 } DP_END_TEST; /* sip_nat10 */
 
+/*
+ * sip_nat11.  Data Set #1. SNAT.
+ *
+ * RTP flow started in forw direction.  RTCP flow started in back direction.
+ */
+DP_DECL_TEST_CASE(sip_nat, sip_nat11, dpt_alg_sipd1_setup,
+		  dpt_alg_sipd1_teardown);
+DP_START_TEST(sip_nat11, test)
+{
+	const char *desc, *pre_pload, *pst_pload;
+	uint hdr_clen, body_clen;
+	bool forw, rv;
+	uint i;
+
+	assert(ARRAY_SIZE(sipd1_pre_snat) == ARRAY_SIZE(sipd1_post_snat));
+	assert(ARRAY_SIZE(sipd1_pre_snat) == ARRAY_SIZE(sipd1_dir));
+
+	/* Configure SNAT with sequential port allocation */
+	struct dp_test_npf_nat_rule_t snat = {
+		.desc		= "snat rule",
+		.rule		= "10",
+		.ifname		= "dp2T1",
+		.proto		= IPPROTO_UDP,
+		.map		= "dynamic",
+		.port_alloc	= "sequential",
+		.from_addr	= "1.1.1.0/24",
+		.from_port	= NULL,
+		.to_addr	= NULL,
+		.to_port	= NULL,
+		.trans_addr	= "30.30.30.2",
+		.trans_port	= "1024-2000",
+	};
+	dp_test_npf_snat_add(&snat, true);
+
+	/* For each SIP msg payload */
+	for (i = 0; i < ARRAY_SIZE(sipd1_pre_snat); i++) {
+		pre_pload = sipd1_pre_snat[i];
+		pst_pload = sipd1_post_snat[i];
+		forw = (sipd1_dir[i] == SIP_FORW);
+		desc = sipd_descr(i, forw, pst_pload);
+
+		/* Check content-length value matches actual content-length */
+		rv = sipd_check_content_length(pre_pload, &hdr_clen,
+					       &body_clen);
+		dp_test_fail_unless(rv, "[%s] Pre  hdr=%u, body=%u",
+				    desc, hdr_clen, body_clen);
+
+		rv = sipd_check_content_length(pst_pload, &hdr_clen,
+					       &body_clen);
+		dp_test_fail_unless(rv, "[%s] Post hdr=%u, body=%u",
+				    desc, hdr_clen, body_clen);
+
+		if (forw) {
+			dpt_udp_pl("dp1T0", "aa:bb:cc:16:0:20",
+				   "1.1.1.2", 5060, "22.22.22.2", 5060,
+				   "30.30.30.2", 1024, "22.22.22.2", 5060,
+				   "aa:bb:cc:18:0:1", "dp2T1",
+				   DP_TEST_FWD_FORWARDED,
+				   pre_pload, strlen(pre_pload),
+				   pst_pload, strlen(pst_pload), desc);
+		} else {
+			dpt_udp_pl("dp2T1", "aa:bb:cc:18:0:1",
+				   "22.22.22.2", 5060, "30.30.30.2", 1024,
+				   "22.22.22.2", 5060, "1.1.1.2", 5060,
+				   "aa:bb:cc:16:0:20", "dp1T0",
+				   DP_TEST_FWD_FORWARDED,
+				   pre_pload, strlen(pre_pload),
+				   pst_pload, strlen(pst_pload), desc);
+		}
+
+		if (i == sipd1_rtp_index) {
+			/* RTP Forw (Initial) */
+			dpt_udp("dp1T0", "aa:bb:cc:16:0:20",
+				"1.1.1.2", 10000, "22.22.22.2", 60000,
+				"30.30.30.2", 1026, "22.22.22.2", 60000,
+				"aa:bb:cc:18:0:1", "dp2T1",
+				DP_TEST_FWD_FORWARDED);
+
+			/* RTP Back */
+			dpt_udp("dp2T1", "aa:bb:cc:18:0:1",
+				"22.22.22.2", 60000, "30.30.30.2", 1026,
+				"22.22.22.2", 60000, "1.1.1.2", 10000,
+				"aa:bb:cc:16:0:20", "dp1T0",
+				DP_TEST_FWD_FORWARDED);
+
+			/* RTCP Back  (Initial) */
+			dpt_udp("dp2T1", "aa:bb:cc:18:0:1",
+				"22.22.22.2", 60001, "30.30.30.2", 1027,
+				"22.22.22.2", 60001, "1.1.1.2", 10001,
+				"aa:bb:cc:16:0:20", "dp1T0",
+				DP_TEST_FWD_FORWARDED);
+
+			/* RTCP Forw */
+			dpt_udp("dp1T0", "aa:bb:cc:16:0:20",
+				"1.1.1.2", 10001, "22.22.22.2", 60001,
+				"30.30.30.2", 1027, "22.22.22.2", 60001,
+				"aa:bb:cc:18:0:1", "dp2T1",
+				DP_TEST_FWD_FORWARDED);
+
+		}
+	}
+
+	dp_test_npf_snat_del(snat.ifname, snat.rule, true);
+
+} DP_END_TEST; /* sip_nat11 */
+
 
 static void dpt_alg_sipd1_setup(void)
 {
