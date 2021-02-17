f3337e194702fcc4354c153345e04dc7aef44783
Derek Fawcus <derek.fawcus@att.com>
2021-02-17 16:55:38 +0000

    ACL: Remove old counter support code
    Jira: VRVDR-54566

--- src/npf/config/pmf_att_rlgrp.h
+++ src/npf/config/pmf_att_rlgrp.h
@@ -8,14 +8,6 @@
 #define PMF_ATT_RLGRP_H
 
 #include <stdio.h>
-#include <stdint.h>
-
-struct pmf_rlset_ext;
-struct pmf_group_ext;
-struct pmf_cntr;
-struct pmf_rule;
-
-struct gpc_group;
 
 void pmf_arlg_init(void);
 void pmf_arlg_commit(void);
@@ -24,20 +16,5 @@ int pmf_arlg_cmd_show_counters(FILE *fp, char const *ifname, int dir,
 				char const *rgname);
 int pmf_arlg_cmd_clear_counters(char const *ifname, int dir,
 				char const *rgname);
-
-struct gpc_group *pmf_arlg_cntr_get_grp(struct pmf_cntr const *eark);
-uintptr_t pmf_arlg_cntr_get_objid(struct pmf_cntr const *eark);
-void pmf_arlg_cntr_set_objid(struct pmf_cntr *eark, uintptr_t objid);
-char const *pmf_arlg_cntr_get_name(struct pmf_cntr const *eark);
-bool pmf_arlg_cntr_pkt_enabled(struct pmf_cntr const *eark);
-bool pmf_arlg_cntr_byt_enabled(struct pmf_cntr const *eark);
-
-/* temporary visibility */
-
-struct gpc_rule;
-void pmf_arlg_hw_ntfy_cntr_add(struct pmf_group_ext *earg,
-			       struct gpc_rule *gprl);
-void pmf_arlg_hw_ntfy_cntr_del(struct pmf_group_ext *earg,
-			       struct gpc_rule *gprl);
 
 #endif /* PMF_ATT_RLGRP_H */
