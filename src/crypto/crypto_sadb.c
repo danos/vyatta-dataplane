8c007e888bfa9b1f57969f6636cbe52c508e7bfc
Srinivas Narayan <narayan@vyatta.att-mail.com>
2021-04-15 14:37:36 +0000

    crypto: Move sa unbinding from PMD to rcu callback
    Removing the association between an SA and the PMD results in
    the removal of the PMD if no more SAs are associated with that PMD.
    This should happen only after the crypto session associated with the
    PMD has been freed.
    
    VRVDR-54873
    
    (cherry picked from commit 93e4658f76cfcfbb09bebc3c00ec1a28a517dfd1)

--- src/crypto/crypto_sadb.c
+++ src/crypto/crypto_sadb.c
@@ -714,16 +714,19 @@ struct sadb_sa *sadb_lookup_inbound(uint32_t spi)
 		return NULL;
 }
 
-static void sadb_sa_destroy(struct sadb_sa *sa)
-{
-	cipher_teardown_ctx(sa);
-	free(sa);
-}
-
 static enum crypto_xfrm crypto_sa_to_xfrm(struct sadb_sa *sa)
 {
 	return sa->dir == CRYPTO_DIR_IN ?
 		CRYPTO_DECRYPT : CRYPTO_ENCRYPT;
+}
+
+static void sadb_sa_destroy(struct sadb_sa *sa)
+{
+	cipher_teardown_ctx(sa);
+	crypto_remove_sa_from_pmd(sa->del_pmd_dev_id,
+				  crypto_sa_to_xfrm(sa),
+				  sa->pending_del);
+	free(sa);
 }
 
 /*
@@ -977,9 +980,6 @@ static void crypto_sadb_del_sa_internal(const xfrm_address_t *dst,
 	if (resurrect_old_sa && !sa->pending_del)
 		crypto_sadb_resurrect_sa(sa, vrf_ctx->vrfid);
 
-	crypto_remove_sa_from_pmd(sa->del_pmd_dev_id,
-				  crypto_sa_to_xfrm(sa),
-				  sa->pending_del);
 	call_rcu(&sa->sa_rcu, sadb_sa_rcu_free);
 	vrf_ctx->count_of_sas--;
 
