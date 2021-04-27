danos/vyatta-dataplane

commit 93e4658f76cfcfbb09bebc3c00ec1a28a517dfd1

crypto: Move sa unbinding from PMD to rcu callback
Removing the association between an SA and the PMD results in
the removal of the PMD if no more SAs are associated with that PMD.
This should happen only after the crypto session associated with the
PMD has been freed.

VRVDR-54873

src/crypto/crypto_sadb.c
@@ -733,16 +733,19 @@ struct sadb_sa *sadb_lookup_inbound(uint32_t spi)
 	return sa;
 }
 
-static void sadb_sa_destroy(struct sadb_sa *sa)
-{
-	cipher_teardown_ctx(sa);
-	free(sa);
-}
-
 static enum crypto_xfrm crypto_sa_to_xfrm(struct sadb_sa *sa)
 {
 	return sa->dir == CRYPTO_DIR_IN ?
 		CRYPTO_DECRYPT : CRYPTO_ENCRYPT;
+}
+
+static void sadb_sa_destroy(struct sadb_sa *sa)
+{
+	cipher_teardown_ctx(sa);
+	crypto_remove_sa_from_pmd(sa->del_pmd_dev_id,
+				  crypto_sa_to_xfrm(sa),
+				  sa->pending_del);
+	free(sa);
 }
 
 /*
@@ -1006,9 +1009,6 @@ static int crypto_sadb_del_sa_internal(const xfrm_address_t *dst,
 	if (resurrect_old_sa && !sa->pending_del)
 		crypto_sadb_resurrect_sa(sa, vrf_ctx->vrfid, sa->reqid);
 
-	crypto_remove_sa_from_pmd(sa->del_pmd_dev_id,
-				  crypto_sa_to_xfrm(sa),
-				  sa->pending_del);
 	crypto_sa_free_fwd_core(sa->fwd_core);
 	call_rcu(&sa->sa_rcu, sadb_sa_rcu_free);
 	vrf_ctx->count_of_sas--;

commit 81e8e85b5134f9e5e908efcbdb5c70fc1386c246

crypto: Rework SA cleanup sequence
Commit 93e4658f76cfcfbb09bebc3c00ec1a28a517dfd1 introduced a
regression due to accidentally moving the PMD cleanup to the RCU
thread. Set up of a PMD is an expensive operation and it is worth
dampening the teardown in scenarios where all SAs are frequently torn
down and brought back up.

VRVDR-54916

src/crypto/crypto.c
@@ -73,6 +73,16 @@ struct cds_list_head;
 struct crypto_pkt_buffer *cpbdb[RTE_MAX_LCORE];
 
 /*
+ * crypto garbage collection timer
+ * Setting up a PMD is an expensive operation. Ideally, PMD setup/teardown
+ * should be triggered by configuration. However, since we create/destroy PMDs
+ * based on SA presence, it is efficient to dampen the deletion to ensure
+ * that a frequent transition of the SA count between zero and 1 does not result
+ * in a lot of unnecessary activity.
+ */
+#define CRYPTO_GC_TIMER_INTERVAL 10
+
+/*
  * The return ring size needs to be a multiple of the the PMD ring, as
  * an RX thread could have many packets queued to many PMD rings.
  */
@@ -81,6 +91,7 @@ struct crypto_pkt_buffer *cpbdb[RTE_MAX_LCORE];
 static struct crypto_dp g_crypto_dp;
 struct crypto_dp *crypto_dp_sp = &g_crypto_dp;
 static struct rte_timer flow_cache_timer;
+static struct rte_timer crypto_gc_timer;
 
 /* between crypto and main thread */
 static zsock_t *crypto_main_pull;
@@ -1602,6 +1613,13 @@ void dp_crypto_init(void)
 	rte_timer_init(&flow_cache_timer);
 	rte_timer_reset(&flow_cache_timer, rte_get_timer_hz(), PERIODICAL,
 			rte_get_master_lcore(), crypto_flow_cache_timer_handler,
+			NULL);
+
+	rte_timer_init(&crypto_gc_timer);
+	rte_timer_reset(&crypto_gc_timer,
+			rte_get_timer_hz() * CRYPTO_GC_TIMER_INTERVAL,
+			PERIODICAL, rte_get_master_lcore(),
+			crypto_gc_timer_handler,
 			NULL);
 
 	CRYPTO_INFO("Crypto initialised\n");

src/crypto/crypto_main.h
@@ -108,4 +108,11 @@ void crypto_create_fwd_queue(unsigned int lcore_id);
 void crypto_destroy_fwd_queue(void);
 void crypto_fwd_processed_packets(void);
 
+/* crypto garbage collection */
+void crypto_gc_timer_handler(struct rte_timer *tmr,
+			     void *arg);
+
+/* Invoked from rcu callback to signal unbind of SA from PMD */
+void crypto_sa_unbind_rcu(int dev_id);
+
 #endif /* _CRYPTO_MAIN_H_ */

src/crypto/crypto_pmd.c
@@ -59,7 +59,7 @@ struct crypto_pmd {
 	struct crypto_pmd_q_pair q_pair;
 	unsigned int lcore;
 	int dev_id;
-	unsigned int sa_cnt;
+	enum cryptodev_type dev_type;
 	int rte_cdev_id;
 	struct rcu_head pmd_rcu;
 	/* --- cacheline 1 boundary (64 bytes) --- */
@@ -71,10 +71,10 @@ struct crypto_pmd {
 	char *padding[0] __rte_cache_aligned;
 	struct pmd_counters cnt[MAX_CRYPTO_XFRM];
 	struct rate_stats rates[MAX_CRYPTO_XFRM];
+	rte_atomic32_t sa_cnt;
 	unsigned int sa_cnt_per_type[MAX_CRYPTO_XFRM];
 	unsigned int pending_remove[MAX_CRYPTO_XFRM];
 	char dev_name[DEV_NAME_LEN];
-	enum cryptodev_type dev_type;
 };
 
 static_assert(offsetof(struct crypto_pmd, padding) == 64,
@@ -160,7 +160,7 @@ pmd_lb_tiebreak(struct crypto_pmd *best_pmd, struct crypto_pmd *pmd,
 
 static int pmd_weighted_sa_cnt(struct crypto_pmd *pmd)
 {
-	return pmd->sa_cnt -
+	return rte_atomic32_read(&pmd->sa_cnt) -
 		crypto_pmd_pend_rm_cnt(pmd, MAX_CRYPTO_XFRM);
 }
 
@@ -406,7 +406,7 @@ int crypto_allocate_pmd(enum crypto_xfrm xfrm,
 		return CRYPTO_PMD_INVALID_ID;
 	}
 
-	pmd->sa_cnt++;
+	rte_atomic32_inc(&pmd->sa_cnt);
 	pmd->sa_cnt_per_type[xfrm]++;
 	pmd_sa_active++;
 
@@ -469,6 +469,41 @@ void crypto_pmd_remove_all(void)
 			crypto_pmd_remove(i);
 }
 
+/*
+ * Invoked from SA cleanup RCU callback to signal completion
+ * of SA deletion. This is to ensure that each PMD gets deleted
+ * only after all SAs associated with it have been freed
+ */
+void crypto_sa_unbind_rcu(int dev_id)
+{
+	bool err;
+	struct crypto_pmd *pmd = crypto_dev_id_to_pmd(dev_id,
+						      &err);
+
+	if (!pmd) {
+		CRYPTO_ERR("No PMD for ID %d\n", dev_id);
+		return;
+	}
+
+	rte_atomic32_dec(&pmd->sa_cnt);
+}
+
+void crypto_gc_timer_handler(struct rte_timer *tmr __rte_unused,
+			     void *arg __rte_unused)
+{
+	struct crypto_pmd *pmd;
+	int i;
+
+	for (i = 0; i < MAX_CRYPTO_PMD; i++) {
+		pmd = crypto_pmd_devs[i];
+		if (!pmd)
+			continue;
+
+		if (!rte_atomic32_read(&pmd->sa_cnt))
+			crypto_pmd_remove(i);
+	}
+}
+
 void crypto_remove_sa_from_pmd(int dev_id, enum crypto_xfrm xfrm,
 			       bool pending)
 {
@@ -483,13 +518,9 @@ void crypto_remove_sa_from_pmd(int dev_id, enum crypto_xfrm xfrm,
 	}
 
 	pmd->sa_cnt_per_type[xfrm]--;
-	pmd->sa_cnt--;
 	pmd_sa_active--;
 	if (pending)
 		crypto_pmd_dec_pending_del(dev_id, xfrm);
-
-	if (!pmd->sa_cnt)
-		crypto_pmd_remove(dev_id);
 }
 
 /*
@@ -596,7 +627,7 @@ crypto_show_pmd_counters(json_writer_t *wr, struct crypto_pmd *pmd)
 	}
 
 	jsonw_string_field(wr, "dev_name", pmd->dev_name);
-	jsonw_uint_field(wr, "active_sa", pmd->sa_cnt);
+	jsonw_uint_field(wr, "active_sa", rte_atomic32_read(&pmd->sa_cnt));
 	jsonw_uint_field(wr, "lcore", pmd->lcore);
 	jsonw_start_array(wr);
 	jsonw_name(wr, "per_pmd_counters");

src/crypto/crypto_sadb.c
@@ -761,6 +761,7 @@ struct sadb_sa *sadb_lookup_inbound(uint32_t spi)
 static void sadb_sa_destroy(struct sadb_sa *sa)
 {
 	cipher_teardown_ctx(sa);
+	crypto_sa_unbind_rcu(sa->del_pmd_dev_id);
 	free(sa);
 }
 