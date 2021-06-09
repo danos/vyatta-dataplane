9a08d4225197c2abf098459e04ae31913ed206e2
Mark Gillott <mgillott@vyatta.att-mail.com>
2021-06-09 15:49:49 +0100

    twamp: UT for twamp offload of IPv4 & IPv6 data messages
    Fixes: VRVDR-53580

--- tests/whole_dp/src/dp_test_twamp.c
+++ tests/whole_dp/src/dp_test_twamp.c
@@ -262,8 +262,373 @@ dp_test_twamp_build_init(TWAMPInitialise *init,
 	free(msg);
 }
 
+static void
+dp_test_twamp_build_pkts(struct rte_mbuf *s, uint32_t sseqno, int s_ttlhop,
+			 struct dp_test_expected *exp, uint32_t rseqno,
+			 uint16_t lport, uint16_t rport,
+			 bool is_v4)
+{
+	struct rte_mbuf *ep;
+	struct udphdr *udp;
+	uint8_t *tpkt;
+	struct timespec sts;
+	struct timespec rts;
+	uint16_t errest_sender = 0;
+	uint16_t errest_reply = 0;
+	void *dont_care_udp_checksum;
+	void *dont_care_ts_1;
+	void *dont_care_ts_2;
+	void *dont_care_errest;
+
+	clock_gettime(CLOCK_REALTIME, &sts);
+	udp = dp_pktmbuf_mtol4(s, struct udphdr *);
+	tpkt = (uint8_t *)(udp + 1);
+
+	*((uint32_t *)tpkt) = htonl(sseqno);
+	tpkt += sizeof(uint32_t);
+	*((uint32_t *)tpkt) = htonl(sts.tv_sec);
+	tpkt += sizeof(uint32_t);
+	*((uint32_t *)tpkt) = htonl(sts.tv_nsec);
+	tpkt += sizeof(uint32_t);
+	*((uint16_t *)tpkt) = htons(errest_sender);
+
+	dp_test_pktmbuf_udp_init(s, rport, lport, is_v4);
+
+	clock_gettime(CLOCK_REALTIME, &rts);
+
+	ep = dp_test_exp_get_pak(exp);
+	udp = dp_pktmbuf_mtol4(ep, struct udphdr *);
+	tpkt = (uint8_t *)(udp + 1);
+
+	*((uint32_t *)tpkt) = htonl(rseqno);
+	tpkt += sizeof(uint32_t);
+
+	dont_care_ts_1 = tpkt;
+	*((uint32_t *)tpkt) = htonl(rts.tv_sec);
+	tpkt += sizeof(uint32_t);
+	*((uint32_t *)tpkt) = htonl(rts.tv_nsec);
+	tpkt += sizeof(uint32_t);
+
+	dont_care_errest = tpkt;
+	*((uint16_t *)tpkt) = htons(errest_reply);
+	tpkt += sizeof(uint16_t);
+	*((uint16_t *)tpkt) = 0;
+	tpkt += sizeof(uint16_t);
+
+	rts.tv_sec++;
+	rts.tv_nsec++;
+
+	dont_care_ts_2 = tpkt;
+	*((uint32_t *)tpkt) = htonl(rts.tv_sec);
+	tpkt += sizeof(uint32_t);
+	*((uint32_t *)tpkt) = htonl(rts.tv_nsec);
+	tpkt += sizeof(uint32_t);
+
+	*((uint32_t *)tpkt) = htonl(sseqno);
+	tpkt += sizeof(uint32_t);
+
+	*((uint32_t *)tpkt) = htonl(sts.tv_sec);
+	tpkt += sizeof(uint32_t);
+	*((uint32_t *)tpkt) = htonl(sts.tv_nsec);
+	tpkt += sizeof(uint32_t);
+
+	*((uint16_t *)tpkt) = htons(errest_sender);
+	tpkt += sizeof(uint16_t);
+	*((uint16_t *)tpkt) = 0;
+	tpkt += sizeof(uint16_t);
+
+	*((uint8_t *)tpkt) = s_ttlhop;
+
+	dp_test_pktmbuf_udp_init(ep, lport, rport, is_v4);
+
+	udp = dp_pktmbuf_mtol4(ep, struct udphdr *);
+	dont_care_udp_checksum = &udp->check;
+
+	/*
+	 * Ignore the UDP checksum and the various timestamp fields in
+	 * the expected reply packet.
+	 */
+	dp_test_exp_set_dont_care(exp, 0, dont_care_udp_checksum, 2);
+	dp_test_exp_set_dont_care(exp, 0, dont_care_ts_1, 8);
+	dp_test_exp_set_dont_care(exp, 0, dont_care_errest, 2);
+	dp_test_exp_set_dont_care(exp, 0, dont_care_ts_2, 8);
+}
+
+static struct dp_test_expected *
+dp_test_twamp_build_ipv6_pkts(struct tw_pkt_desc *pkt,
+			      struct rte_mbuf **send_pak)
+{
+	struct rte_mbuf *ep;
+	struct ip6_hdr *ip6;
+	struct dp_test_expected *exp;
+	struct rte_mbuf *s_pak;
+	struct rte_mbuf *r_pak;
+	int s_hlim = 255;
+	int r_hlim = 255;
+
+	s_pak = dp_test_create_raw_ipv6_pak(pkt->raddr.addrstr, pkt->laddr.addrstr,
+					    IPPROTO_UDP,
+					    1, &pkt->sndpktlen);
+	r_pak = dp_test_create_raw_ipv6_pak(pkt->laddr.addrstr, pkt->raddr.addrstr,
+					    IPPROTO_UDP,
+					    1, &pkt->rcvpktlen);
+	ip6 = ip6hdr(s_pak);
+	ip6->ip6_hlim = s_hlim;
+	ip6 = ip6hdr(r_pak);
+	ip6->ip6_hlim = r_hlim;
+
+	exp = dp_test_exp_create(r_pak);
+	dp_test_assert_internal(exp != NULL);
+	dp_test_exp_set_fwd_status(exp, DP_TEST_FWD_FORWARDED);
+	dp_test_exp_set_oif_name(exp, pkt->oif);
+	rte_pktmbuf_free(r_pak);
+
+	dp_test_twamp_build_pkts(s_pak, pkt->sseqno, s_hlim, exp, pkt->rseqno,
+				 pkt->lport, pkt->rport, false);
+
+	ep = dp_test_exp_get_pak(exp);
+	(void)dp_test_pktmbuf_eth_init(s_pak,
+				       dp_test_intf_name2mac_str(pkt->oif),
+				       pkt->macaddr, RTE_ETHER_TYPE_IPV6);
+	(void)dp_test_pktmbuf_eth_init(ep,
+				       pkt->macaddr,
+				       dp_test_intf_name2mac_str(pkt->oif),
+				       RTE_ETHER_TYPE_IPV6);
+
+	dp_test_assert_internal(ip6_valid_packet(s_pak, ip6hdr(s_pak)));
+	dp_test_assert_internal(ip6_valid_packet(ep, ip6hdr(ep)));
+
+	*send_pak = s_pak;
+	return exp;
+}
+
+static struct dp_test_expected *
+dp_test_twamp_build_ipv4_pkts(struct tw_pkt_desc *pkt,
+			      struct rte_mbuf **send_pak,
+			      bool addopts)
+{
+	struct rte_mbuf *ep;
+	struct iphdr *ip;
+	struct dp_test_expected *exp;
+	struct rte_mbuf *s_pak;
+	struct rte_mbuf *r_pak;
+	int s_ttl = 255;
+	int r_ttl = 255;
+	int s_pktlen = pkt->sndpktlen;
+	int optlen = 4;
+
+	if (addopts)
+		s_pktlen += optlen;
+
+	s_pak = dp_test_create_raw_ipv4_pak(pkt->raddr.addrstr, pkt->laddr.addrstr,
+					    IPPROTO_UDP,
+					    1, &s_pktlen);
+	rte_pktmbuf_adj(s_pak, s_pak->l2_len);
+	s_pak->l2_len = 0;
+	r_pak = dp_test_create_raw_ipv4_pak(pkt->laddr.addrstr, pkt->raddr.addrstr,
+					    IPPROTO_UDP,
+					    1, &pkt->rcvpktlen);
+
+	ip = iphdr(r_pak);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TTL, r_ttl);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TOS,
+				 IPTOS_PREC_INTERNETCONTROL);
+	ip = iphdr(s_pak);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TTL, s_ttl);
+	dp_test_set_pak_ip_field(ip, DP_TEST_SET_TOS,
+				 IPTOS_PREC_INTERNETCONTROL);
+
+	if (addopts) {
+		uint8_t *opts;
+		void *newip;
+
+		newip = rte_pktmbuf_prepend(s_pak, optlen);
+		dp_test_assert_internal(newip != NULL);
+		memmove(newip, ip, sizeof(*ip));
+		ip = newip;
+
+		opts = (uint8_t *)(ip + 1);
+		opts[IPOPT_OPTVAL] = IPOPT_NOP;
+		opts++;
+		opts[IPOPT_OPTVAL] = IPOPT_NOP;
+		opts++;
+		opts[IPOPT_OPTVAL] = IPOPT_NOP;
+		opts++;
+		opts[IPOPT_OPTVAL] = IPOPT_EOL;
+		ip->ihl = (sizeof(*ip) + optlen) / 4;
+		ip->check = 0;
+		ip->check = ip_checksum(ip, ip->ihl * 4);
+		dp_pktmbuf_l3_len(s_pak) = sizeof(*ip) + optlen;
+		dp_test_assert_internal(ip_valid_packet(s_pak, iphdr(s_pak)));
+	}
+
+	exp = dp_test_exp_create(r_pak);
+	dp_test_assert_internal(exp != NULL);
+	dp_test_exp_set_fwd_status(exp, DP_TEST_FWD_FORWARDED);
+	dp_test_exp_set_oif_name(exp, pkt->oif);
+	rte_pktmbuf_free(r_pak);
+
+	dp_test_twamp_build_pkts(s_pak, pkt->sseqno, s_ttl, exp, pkt->rseqno,
+				 pkt->lport, pkt->rport, true);
+
+	ep = dp_test_exp_get_pak(exp);
+	(void)dp_test_pktmbuf_eth_prepend(s_pak,
+				       dp_test_intf_name2mac_str(pkt->oif),
+				       pkt->macaddr, RTE_ETHER_TYPE_IPV4);
+	(void)dp_test_pktmbuf_eth_init(ep,
+				       pkt->macaddr,
+				       dp_test_intf_name2mac_str(pkt->oif),
+				       RTE_ETHER_TYPE_IPV4);
+
+	dp_test_assert_internal(ip_valid_packet(s_pak, iphdr(s_pak)));
+	dp_test_assert_internal(ip_valid_packet(ep, iphdr(ep)));
+
+	*send_pak = s_pak;
+	return exp;
+}
+
 DP_DECL_TEST_SUITE(twamp_offload_suite);
 
+DP_DECL_TEST_CASE(twamp_offload_suite, twamp_rxtx, NULL, NULL);
+
+DP_START_TEST(twamp_rxtx, ip6)
+{
+	TWAMPSessionCreate create;
+	TWAMPSessionCounters counters;
+	TWAMPSessionDelete delete;
+	TWAMPSessionKey key;
+	struct dp_test_twamp_response resp;
+	struct dp_test_expected *exp_pak;
+	struct rte_mbuf *s_pak;
+	struct tw_pkt_desc pkt = {
+		.laddr.addrstr = "2001:1::1",
+		.raddr.addrstr = "2001:1::3",
+		.lport = PORT2,
+		.rport = PORT3,
+		.macaddr = "aa:bb:cc:dd:ee:ff",
+		.oif = "dp1T0",
+		.sndpktlen = DEFAULT_PKT_SIZE,
+		.rcvpktlen = DEFAULT_PKT_SIZE,
+	};
+
+	dp_test_nl_add_ip_addr_and_connected("dp1T0", "2001:1::1/96");
+	dp_test_netlink_add_neigh("dp1T0", pkt.raddr.addrstr, pkt.macaddr);
+
+	exp_pak = dp_test_twamp_build_ipv6_pkts(&pkt, &s_pak);
+	pkt.sseqno++;
+	pkt.rseqno++;
+
+	dp_test_twamp_build_key(&key, &pkt.laddr, &pkt.raddr,
+				pkt.lport, pkt.rport);
+	TWAMP_CREATE_DEFAULT(&create, &key, &resp);
+	dp_test_assert_internal(resp.status == 0);
+
+	dp_test_pak_receive(s_pak, pkt.oif, exp_pak);
+
+	dp_test_twamp_build_counters(&counters, &key, &resp);
+	dp_test_assert_internal(resp.status == 0);
+	dp_test_assert_internal(resp.has_counters == 1);
+	dp_test_assert_internal(resp.tx_pkts == 1);
+	dp_test_assert_internal(resp.rx_pkts == 1);
+
+	json_object *jobj = dp_test_json_create(
+		"{\"twamp-sessions\":"
+		"["
+		"{\"local-port\":%d,\"remote-port\":%d,"
+		" \"local-address\":\"%s\",\"remote-address\":\"%s\","
+		" \"rx-pkts\":%d}"
+		"]}",
+		pkt.lport, pkt.rport, pkt.laddr.addrstr, pkt.raddr.addrstr, 1);
+	dp_test_check_json_state("vyatta:twamp dump", jobj,
+				 DP_TEST_JSON_CHECK_SUBSET, false);
+	json_object_put(jobj);
+
+	dp_test_twamp_build_delete(&delete, &key, &resp);
+	dp_test_assert_internal(resp.status == 0);
+
+	json_object *expected = dp_test_json_create("{\"twamp-sessions\":[]}");
+	dp_test_check_json_state("vyatta:twamp dump", expected,
+				 DP_TEST_JSON_CHECK_EXACT, false);
+	json_object_put(expected);
+
+	dp_test_netlink_del_neigh("dp1T0", pkt.raddr.addrstr, pkt.macaddr);
+	dp_test_nl_del_ip_addr_and_connected("dp1T0", "2001:1::1/96");
+} DP_END_TEST;
+
+DP_START_TEST(twamp_rxtx, ip4)
+{
+	TWAMPSessionCreate create;
+	TWAMPSessionCounters counters;
+	TWAMPSessionDelete delete;
+	TWAMPSessionKey key;
+	struct dp_test_twamp_response resp;
+	struct dp_test_expected *exp_pak;
+	struct rte_mbuf *s_pak;
+	struct tw_pkt_desc pkt = {
+		.laddr.addrstr = "1.1.1.1",
+		.raddr.addrstr = "1.1.1.3",
+		.lport = PORT2,
+		.rport = PORT3,
+		.macaddr = "aa:bb:cc:dd:ee:ff",
+		.oif = "dp1T0",
+		.sndpktlen = DEFAULT_PKT_SIZE,
+		.rcvpktlen = DEFAULT_PKT_SIZE,
+	};
+
+	dp_test_nl_add_ip_addr_and_connected("dp1T0", "1.1.1.1/24");
+	dp_test_netlink_add_neigh("dp1T0", pkt.raddr.addrstr, pkt.macaddr);
+
+	exp_pak = dp_test_twamp_build_ipv4_pkts(&pkt, &s_pak, false);
+	pkt.sseqno++;
+	pkt.rseqno++;
+
+	dp_test_twamp_build_key(&key, &pkt.laddr, &pkt.raddr,
+				pkt.lport, pkt.rport);
+	TWAMP_CREATE_DEFAULT(&create, &key, &resp);
+	dp_test_assert_internal(resp.status == 0);
+
+	dp_test_pak_receive(s_pak, pkt.oif, exp_pak);
+
+	dp_test_twamp_build_counters(&counters, &key, &resp);
+	dp_test_assert_internal(resp.status == 0);
+	dp_test_assert_internal(resp.has_counters == 1);
+	dp_test_assert_internal(resp.tx_pkts == 1);
+	dp_test_assert_internal(resp.rx_pkts == 1);
+
+	json_object *jobj = dp_test_json_create(
+		"{\"twamp-sessions\":"
+		"["
+		"{\"local-port\":%d,\"remote-port\":%d,"
+		" \"local-address\":\"%s\",\"remote-address\":\"%s\","
+		" \"rx-pkts\":%d}"
+		"]}",
+		pkt.lport, pkt.rport, pkt.laddr.addrstr, pkt.raddr.addrstr, 1);
+	dp_test_check_json_state("vyatta:twamp dump", jobj,
+				 DP_TEST_JSON_CHECK_SUBSET, false);
+	json_object_put(jobj);
+
+	exp_pak = dp_test_twamp_build_ipv4_pkts(&pkt, &s_pak, true);
+	pkt.sseqno++;
+	pkt.rseqno++;
+	dp_test_pak_receive(s_pak, pkt.oif, exp_pak);
+
+	dp_test_twamp_build_counters(&counters, &key, &resp);
+	dp_test_assert_internal(resp.status == 0);
+	dp_test_assert_internal(resp.has_counters == 1);
+	dp_test_assert_internal(resp.tx_pkts == 2);
+	dp_test_assert_internal(resp.rx_pkts == 2);
+
+	dp_test_twamp_build_delete(&delete, &key, &resp);
+	dp_test_assert_internal(resp.status == 0);
+
+	json_object *expected = dp_test_json_create("{\"twamp-sessions\":[]}");
+	dp_test_check_json_state("vyatta:twamp dump", expected,
+				 DP_TEST_JSON_CHECK_EXACT, false);
+	json_object_put(expected);
+
+	dp_test_netlink_del_neigh("dp1T0", pkt.raddr.addrstr, pkt.macaddr);
+	dp_test_nl_del_ip_addr_and_connected("dp1T0", "1.1.1.1/24");
+} DP_END_TEST;
 
 DP_DECL_TEST_CASE(twamp_offload_suite, twamp_session, NULL, NULL);
 
