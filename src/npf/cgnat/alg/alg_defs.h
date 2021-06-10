613b3863fafab45ceba7bc70bb1cda79ca41c389
Ian Wilson <ian.wilson@att.com>
2021-06-10 20:01:41 +0100

    cgnat-alg: Re-order alg ID enum
    Re-order into same order as the ALGs will be added to CGNAT
    VRVDR-55350

--- src/npf/cgnat/alg/alg_defs.h
+++ src/npf/cgnat/alg/alg_defs.h
@@ -12,19 +12,19 @@
  */
 enum cgn_alg_id {
 	CGN_ALG_NONE,
-	CGN_ALG_FTP,
 	CGN_ALG_PPTP,
 	CGN_ALG_SIP,
+	CGN_ALG_FTP,
 } __attribute__ ((__packed__));
 
-#define CGN_ALG_FIRST	CGN_ALG_FTP
-#define CGN_ALG_LAST	CGN_ALG_SIP
+#define CGN_ALG_FIRST	CGN_ALG_PPTP
+#define CGN_ALG_LAST	CGN_ALG_FTP
 #define CGN_ALG_MAX	(CGN_ALG_LAST + 1)
 
 #define CGN_ALG_BIT(id)		(1 << ((id) - 1))
-#define CGN_ALG_BIT_FTP		CGN_ALG_BIT(CGN_ALG_FTP)
 #define CGN_ALG_BIT_PPTP	CGN_ALG_BIT(CGN_ALG_PPTP)
 #define CGN_ALG_BIT_SIP		CGN_ALG_BIT(CGN_ALG_SIP)
+#define CGN_ALG_BIT_FTP		CGN_ALG_BIT(CGN_ALG_FTP)
 
 /*
  * Assert as zero so we can simply test for zero or non-zero to determine if a
