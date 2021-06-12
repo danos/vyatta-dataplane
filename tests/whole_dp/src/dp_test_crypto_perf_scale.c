3353f8fdb10e6079254254e03d0004b3b4d84bf4
Daniel Gollub <dgollub@att.com>
2021-06-12 02:52:30 -0700

    ut: dp_test_crypto_perf_scale perform policy batch commits
    And increase the policy/scale count.
    
    VRVDR-47960

--- tests/whole_dp/src/dp_test_crypto_perf_scale.c
+++ tests/whole_dp/src/dp_test_crypto_perf_scale.c
@@ -41,17 +41,15 @@
 #define SOURCE_IP_ADDR  "1.1.1.1"
 #define SOURCE_MAC_ADDR "aa:bb:cc:dd:1:1"
 
+#define NETWORK_BASE_OUT_ADDR 0x14000000
+#define NETWORK_BASE_IN_ADDR 0x15000000
+
 /*
  * Tunnel 1 parameters
  */
 #define TUN_1_LOCAL_IP_ADDR   "2.2.2.2"
 #define TUN_1_REMOTE_IP_ADDR  "2.2.2.3"
 #define TUN_1_REMOTE_MAC_ADDR "aa:bb:cc:dd:2:3"
-#define TUN_1_LOCAL_PREFIX "1.%d.%d.0/24"
-#define TUN_1_REMOTE_PREFIX "8.%d.%d.0/24"
-#define TUN_1_SINK_IP_ADDR  "8.8.8.8"
-#define TUN_1_IN_SA_SPI   0x22223333
-#define TUN_1_OUT_SA_SPI  0x33332222
 #define TUN_1_REQID_START 0x1111
 
 #define TUN_1_EXTRA_OUT_SA_SPI 0x10002000
@@ -62,8 +60,6 @@
  *   1.1.1.0/2 -[2.2.2.2]========[2.2.2.3] -- 8.8.8.0/24
  */
 static struct dp_test_crypto_policy tun_1_in_policy = {
-	.d_prefix = TUN_1_LOCAL_PREFIX,
-	.s_prefix = TUN_1_REMOTE_PREFIX,
 	.proto = 0,
 	.dst = TUN_1_LOCAL_IP_ADDR,
 	.dst_family = AF_INET,
@@ -76,8 +72,6 @@ static struct dp_test_crypto_policy tun_1_in_policy = {
 };
 
 static struct dp_test_crypto_policy tun_1_out_policy = {
-	.d_prefix = TUN_1_REMOTE_PREFIX,
-	.s_prefix = TUN_1_LOCAL_PREFIX,
 	.proto = 0,
 	.dst = TUN_1_REMOTE_IP_ADDR,
 	.dst_family = AF_INET,
@@ -111,63 +105,83 @@ static void setup(vrfid_t vrfid)
 				  TUN_1_REMOTE_MAC_ADDR);
 }
 
-static void setup_or_teardown_tunnels(uint32_t tunnel_cnt, bool setup)
+static void setup_or_teardown_tunnels(uint32_t tunnel_cnt,
+				      uint32_t policy_batch_size, bool setup)
 {
 #define PREFIX_SIZE 20
 	char local_prefix[PREFIX_SIZE];
 	char remote_prefix[PREFIX_SIZE];
-	uint8_t i, j;
+	char ip_peer_addr_str[INET_ADDRSTRLEN];
+	uint32_t i;
+	uint32_t tmp_ip;
+	uint32_t ip_peer_addr_out = NETWORK_BASE_OUT_ADDR;
+	uint32_t ip_peer_addr_in = NETWORK_BASE_IN_ADDR;
 	uint32_t reqid = TUN_1_REQID_START;
 	uint32_t rule_no = 0;
-	struct timespec start, end;
+	struct timespec total_start, start, end;
 	uint64_t ptime = 0;
-	uint32_t outer = tunnel_cnt / 253;
-	uint32_t inner = 253;
-	uint64_t cur_cnt;
+	uint64_t cur_cnt, batch_cnt = 0;
+	bool do_commit = false;
 
-	if (tunnel_cnt < 253) {
-		outer = 1;
-		inner = tunnel_cnt;
-	}
-
+	clock_gettime(CLOCK_REALTIME, &total_start);
 	clock_gettime(CLOCK_REALTIME, &start);
 
 	cur_cnt = 0;
-	for (i = 1; i <= outer; i++) {
-		for (j = 1; j <= inner; j++) {
-			snprintf(local_prefix, PREFIX_SIZE,
-				 TUN_1_LOCAL_PREFIX, i, j);
-			snprintf(remote_prefix, PREFIX_SIZE,
-				 TUN_1_REMOTE_PREFIX, i, j);
-			tun_1_in_policy.s_prefix = remote_prefix;
-			tun_1_in_policy.d_prefix = local_prefix;
-			tun_1_in_policy.reqid = reqid;
-			tun_1_in_policy.rule_no = ++rule_no;
+	for (i = 0; i < tunnel_cnt; i++) {
+		tmp_ip = htonl(ip_peer_addr_in++);
+		if (!inet_ntop(AF_INET, &tmp_ip,
+			       ip_peer_addr_str, INET_ADDRSTRLEN))
+			assert(0);
+		snprintf(local_prefix, sizeof(local_prefix), "%s/%u",
+			 ip_peer_addr_str, 32);
 
-			tun_1_out_policy.s_prefix = local_prefix;
-			tun_1_out_policy.d_prefix = remote_prefix;
-			tun_1_out_policy.reqid = reqid++;
-			tun_1_out_policy.rule_no = ++rule_no;
+		tmp_ip = htonl(ip_peer_addr_out++);
+		if (!inet_ntop(AF_INET, &tmp_ip,
+			       ip_peer_addr_str, INET_ADDRSTRLEN))
+			assert(0);
+		snprintf(remote_prefix, sizeof(remote_prefix), "%s/%u",
+			 ip_peer_addr_str, 32);
 
-			if (setup) {
-				dp_test_crypto_create_policy_verify(
-					&tun_1_in_policy, false);
-				dp_test_crypto_create_policy_verify(
-					&tun_1_out_policy, false);
-			} else {
-				dp_test_crypto_delete_policy_verify(
-					&tun_1_in_policy, false);
-				dp_test_crypto_delete_policy_verify(
-					&tun_1_out_policy, false);
-			}
+		tun_1_in_policy.s_prefix = remote_prefix;
+		tun_1_in_policy.d_prefix = local_prefix;
+		tun_1_in_policy.reqid = reqid;
+		tun_1_in_policy.rule_no = ++rule_no;
+
+		tun_1_out_policy.s_prefix = local_prefix;
+		tun_1_out_policy.d_prefix = remote_prefix;
+		tun_1_out_policy.reqid = reqid++;
+		tun_1_out_policy.rule_no = ++rule_no;
+
+		cur_cnt += 2;
+
+		if (i == tunnel_cnt-1 || cur_cnt % policy_batch_size == 0)
+			do_commit = true;
+		else
+			do_commit = false;
+
+		if (setup) {
+			dp_test_crypto_create_policy_commit(
+				&tun_1_in_policy, false);
+			dp_test_crypto_create_policy_commit(
+				&tun_1_out_policy, do_commit);
+		} else {
+			dp_test_crypto_delete_policy_commit(
+				&tun_1_in_policy, false);
+			dp_test_crypto_delete_policy_commit(
+				&tun_1_out_policy, do_commit);
 		}
-		cur_cnt += inner * 2;
+
+		if (do_commit) {
+			clock_gettime(CLOCK_REALTIME, &end);
+			ptime = timespec_diff_us(&start, &end);
+			printf("Time taken to %s %u/%lu policies (batch #%lu) = %lu us\n",
+			       setup ? "create" : "delete",
+			       policy_batch_size, cur_cnt,
+			       ++batch_cnt, ptime);
+			clock_gettime(CLOCK_REALTIME, &start);
+		}
 	}
 
-	clock_gettime(CLOCK_REALTIME, &end);
-	ptime = timespec_diff_us(&start, &end);
-	printf("Time taken to request %s of %lu policies = %lu us\n",
-	       setup ? "creation" : "deletion", cur_cnt, ptime);
 
 	dp_test_crypto_check_policy_count(VRF_DEFAULT_ID,
 					  setup ? cur_cnt : 0, AF_INET);
@@ -176,7 +190,7 @@ static void setup_or_teardown_tunnels(uint32_t tunnel_cnt, bool setup)
 		dp_test_npf_cleanup();
 
 	clock_gettime(CLOCK_REALTIME, &end);
-	ptime = timespec_diff_us(&start, &end);
+	ptime = timespec_diff_us(&total_start, &end);
 	printf("Time taken to %s %lu policies = %lu us\n",
 	       setup ? "install" : "delete", cur_cnt, ptime);
 }
@@ -215,11 +229,12 @@ DP_START_TEST_FULL_RUN(crypto_policy_scale, policy_update_scale)
 	if (debug)
 		dp_test_console_request_reply("debug rldb-acl", true);
 
-#define MAX_TUNNEL_CNT 64
+#define POLICY_BATCH_SIZE 512
+#define MAX_TUNNEL_CNT (1 << 15)
 	setup(VRF_DEFAULT_ID);
 
-	setup_or_teardown_tunnels(MAX_TUNNEL_CNT, true);
-	setup_or_teardown_tunnels(MAX_TUNNEL_CNT, false);
+	setup_or_teardown_tunnels(MAX_TUNNEL_CNT, POLICY_BATCH_SIZE, true);
+	setup_or_teardown_tunnels(MAX_TUNNEL_CNT, POLICY_BATCH_SIZE, false);
 
 	teardown(VRF_DEFAULT_ID);
 
